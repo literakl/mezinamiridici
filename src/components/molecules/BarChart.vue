b3f18e7dcaf4f45d1a8d12823b7beb275f28940d
Jacob Clark <jacob.jh.clark@googlemail.com>
2019-09-08 18:13:32 +0100

    Chart - desktop only

---
+++ src/components/molecules/BarChart.vue
@@ -0,0 +1,108 @@
+<template>
+    <div>  
+        <table id="barchart__table">
+            <tbody>
+                <tr class="qtr" id="q1">
+                    <th scope="row">No Problem</th>
+                    <td class="sent bar" style="height: 111px;">  
+                        <p>13%</p>
+                    </td>
+                </tr>
+                <tr class="qtr" id="q2">
+                    <th scope="row">Trivial Trouble</th>
+                    <td class="sent bar" style="height: 206px;">
+                        <p>40%</p>
+                    </td>
+                </tr>
+                <tr class="qtr" id="q3">
+                    <th scope="row">I don't like it</th>
+                    <td class="sent bar" style="height: 300px; background-color: #ffd302">
+                        <p>90%</p>
+                    </td>
+                </tr>
+                <tr class="qtr" id="q4">
+                    <th scope="row">I hate it</th>
+                    <td class="sent bar" style="height: 110px;">
+                        <p>30%</p>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'BarChart',
+  components: {
+  },
+  data: () => ({
+  }),
+  methods: {
+  },
+};
+</script>
+
+
+<style lang="scss">
+#barchart__table {
+  display: block; /* fixes layout wonkiness in FF1.5 */
+  position: relative; 
+  width: 750px; 
+  height: 320px;
+  margin: 0 0 30px 0;
+  padding: 0;
+  font-size: 20px;
+  border-bottom: 1px solid black;
+
+}
+
+#barchart__table tr, 
+#barchart__table td { 
+  position: absolute;
+  bottom: 0; 
+  width: 60px; 
+  z-index: 2;
+  margin: 0; 
+  padding: 0;
+  text-align: center;
+}
+
+#barchart__table th {
+    position: absolute;
+  bottom: 0; 
+  width: 60px; 
+  z-index: 2;
+  margin: 20px 0 0 0; 
+  padding: 0;
+  text-align: center; 
+}
+
+#q1 {left: 0;}
+#q2 {left: 200px;}
+#q3 {left: 400px;}
+#q4 {left: 600px; border-right: none;}
+
+#barchart__table tbody th {
+  bottom: -1.75em;
+  width: 150px; 
+  vertical-align: top;
+  font-weight: normal; 
+  color: #333;
+}
+
+#barchart__table .bar {
+  width: 60px; 
+  margin-left:45px;
+}
+
+#barchart__table .bar p {
+  margin: -25px 0 0; 
+}
+
+#barchart__table .sent {
+  background-color: black;
+  border-color: transparent;
+  border-radius: 5px;
+}
+</style>
--- src/views/Poll.vue
+++ src/views/Poll.vue
@@ -1,29 +1,41 @@
 <template>
     <div id="poll__wrapper">
-        <div>
+        <div id="poll__ads">
+          Ads
+        </div>
+        <div id="poll_content">
           <Heading />
-          <h2>Your vote</h2>
-          <div>Chart will go here</div>
-          <Button value="Analyze votes" />
-          
           <div>
-            <h2>Discussion (440)</h2>
-            <p>What do you want to say about it?</p>
-            <form>
-              <textarea />
-              <Submit value="Send" />
-            </form>
+            <div id="poll__chart-wrapper">
+              <h2 class="poll__chart-wrapper-vote">
+                Your vote: 
+                <span class="vote-text">I don't like it</span>
+              </h2>
+              <div class="poll__chart-wrapper-bar-chart">
+                <BarChart />
+              </div>
+              <Button class="poll__chart-wrapper-analyze-votes-button" value="Analyze votes" />
+            </div>
+            <div>
+              <h2>Discussion (440)</h2>
+              <p>What do you want to say about it?</p>
+              <form>
+                <textarea />
+                <Submit value="Send" />
+              </form>
+            </div>
+
+            <hr />
+
+            <h2><router-link to="/polls">Other polls</router-link></h2>
           </div>
-
-          <hr />
-
-          <h2><router-link to="/polls">Other polls</router-link></h2>
         </div>
     </div>
 </template>
 
 <script>
 import Heading from '@/components/molecules/Heading.vue'
+import BarChart from '@/components/molecules/BarChart.vue'
 import Button from '@/components/atoms/Button.vue'
 import Submit from '@/components/atoms/Submit.vue'
 
@@ -32,7 +44,8 @@ export default {
   components: {
     Heading,
     Button,
-    Submit
+    Submit,
+    BarChart
   },
 };
 </script>
@@ -47,9 +60,52 @@ export default {
     grid-gap: 20px;
 }
 
+#poll__chart-wrapper {
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  display: grid;
+  padding: 1em 0;
+  grid-gap: 20px;
+}
+
+.poll__chart-wrapper-vote {
+  grid-column: 1 / span 4
+}
+
+.poll__chart-wrapper-bar-chart {
+  grid-column: 1 / span 4;
+  margin: 0 auto;
+}
+
+.poll__chart-wrapper-analyze-votes-button {
+  grid-column: 1 / span 4;
+  margin: 0 auto;
+}
+
+#poll_content {
+  order: 2;
+}
+
+#poll__ads {
+  height: 100%;
+  background: grey;
+  order: 1;
+}
+
+.vote-text {
+  color: red;
+}
+
 @media all and (min-width: 850px) {
   #poll__wrapper {
     grid-template-columns: 1fr 0.2fr;
+  }
+
+  #poll_content {
+    order: 1;
+  }
+
+  #poll__ads {
+    order: 2;
   }
 }
 </style>
