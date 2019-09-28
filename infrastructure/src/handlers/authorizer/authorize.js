eb261af8c2305fa700d165660eab745ade051f80
Jacob Clark <jacob.jh.clark@googlemail.com>
2019-09-28 17:39:55 +0100

    CORS preflight

--- infrastructure/src/handlers/authorizer/authorize.js
+++ infrastructure/src/handlers/authorizer/authorize.js
@@ -6,12 +6,12 @@ const generatePolicy = function(principalId, effect, resource) {
     authResponse.principalId = principalId;
     if (effect && resource) {
         const policyDocument = {
-            'Version': '2012-10-17',
-            'Statement': [
-                'Action': 'execute-api:Invoke',
-                'Effect': effect,
-                'Resource': resource
-            ]
+            "Version": "2012-10-17",
+            "Statement": [{
+                "Action": "execute-api:Invoke",
+                "Effect": effect,
+                "Resource": resource
+            }]
         };
 
         authResponse.policyDocument = policyDocument;
