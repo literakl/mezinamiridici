57236c2339e8850ff9a7ae3fefa4d27315d72212
Leos Literak <leos@literak.cz>
2020-01-07 19:28:02 +0100

    turn off too long line errors

--- spa/.eslintrc.js
+++ spa/.eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'max-len': [2, 190, 4],
+    'max-len': [0, 190, 4],
     'linebreak-style': [0, "windows"]
   },
   parserOptions: {
