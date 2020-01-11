2c59a547ee091a37f2b6fe07fdde1dc1c33bd237
Leos Literak <leos@literak.cz>
2020-01-11 12:25:19 +0100

    experimental connection to Mongo Atlas

--- infrastructure/src/handlers/users/authorizeUser.js
+++ infrastructure/src/handlers/users/authorizeUser.js
@@ -20,8 +20,62 @@ const response = (status, body) => {
     }
 }
 
+const MongoClient = require('mongodb').MongoClient;
+const MONGODB_URI = "mongodb+srv://literakl:CgTqEq4nkgLolm5i@atlas-ozgwo.mongodb.net/test?retryWrites=true&w=majority"; // or Atlas connection string
+
+let cachedDb = null;
+
+function connectToDatabase (uri) {
+    console.log('=> connect to database');
+
+    if (cachedDb) {
+        console.log('=> using cached database instance');
+        return Promise.resolve(cachedDb);
+    }
+
+    return MongoClient.connect(uri)
+        .then(db => {
+            cachedDb = db;
+            return cachedDb;
+        });
+}
+
+function queryDatabase (db) {
+    console.log('=> query database');
+
+    return db.collection('items').find({}).toArray()
+        .then(() => { return { statusCode: 200, body: 'success' }; })
+        .catch(err => {
+            console.log('=> an error occurred: ', err);
+            return { statusCode: 500, body: 'error' };
+        });
+}
+
+function insertUser(db, email) {
+    console.log('=> modify database');
+    return db.collection('users').insertOne({"email" : email})
+        .then(() => { return { statusCode: 200, body: 'success' }; })
+        .catch(err => {
+            console.log('=> an error occurred: ', err);
+            return { statusCode: 500, body: 'error' };
+        });
+}
+
 exports.handler = (payload, context, callback) => {
     const { email, password } = JSON.parse(payload.body);
+
+    context.callbackWaitsForEmptyEventLoop = false;
+    connectToDatabase(MONGODB_URI)
+        .then(db => insertUser(db, email))
+        .then(result => {
+            console.log('=> returning result: ', result);
+            // callback(null, result);
+        })
+        .catch(err => {
+            console.log('=> an error occurred: ', err);
+            return responses.INTERNAL_SERVER_ERROR_500(err, callback, response);
+        });
+    console.log('mongo insert succeeded');
 
     dynamodb.query({
         "TableName": "BUDUserTable",
