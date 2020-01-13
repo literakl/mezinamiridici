fd12a6eac9117279b8f93bd7e185341412d60660
Leos Literak <leos@literak.cz>
2020-01-13 21:20:34 +0100

    code cleanup, more logs, preparation for putting local config to environment

--- infrastructure/src/handlers/users/authorizeUser.js
+++ infrastructure/src/handlers/users/authorizeUser.js
@@ -4,22 +4,13 @@ const MongoClient = require('mongodb').MongoClient;
 
 const responses = require('../../utils/responses.js');
 
+// TODO this needs to be externalized
 const SECRET = 'betweenusdrivers2019';
 
-const response = (status, body) => {
-    return {
-        "statusCode": status,
-        "headers": {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-            "Cache-Control": "private"
-        },
-        "body": JSON.stringify(body),
-        "isBase64Encoded": false
-    }
-}
-
 const MONGODB_URI = "mongodb+srv://literakl:CgTqEq4nkgLolm5i@atlas-ozgwo.mongodb.net/bud?retryWrites=true&w=majority";
+// TODO we need to externalize MongoURI URI
+console.log("Mongo parameter: " + process.env.MONGODB_URI);
+console.log("Process environment" + process.env);
 
 let cachedDb = null;
 
@@ -40,16 +31,6 @@ function connectToDatabase(uri) {
         .catch(err => {
             console.log("Connection error occurred: ", err);
             throw err;
-        });
-}
-
-function insertUser(dbClient, email) {
-    console.log("insertUser");
-    return dbClient.db()
-        .collection("users")
-        .insertOne({ email: email, created: new Date() })
-        .catch(err => {
-            console.log("Insert error occurred: ", err);
         });
 }
 
@@ -79,28 +60,27 @@ exports.handler = (payload, context, callback) => {
             return findUser(db, email);
         })
         .then(user => {
-            console.log("Mongo search succeeded");
+            console.log("User was found"); // following part takes more than 1 second!
 
             // if(!user.verified){
-            //     return responses.FORBIDDEN_403(callback, response);
+            //     return responses.FORBIDDEN_403(callback, responses.sendRresponse);
             // }
 
             if (bcrypt.compareSync(password, user.password)) {
+                console.log("Password verified"); //
                 const token = jwt.sign({
                     "userId": user.email,
                     "nickname": user.nickname
                 }, SECRET);
                 console.log("All good");
-                return responses.OK_200({token}, callback, response)
+                return responses.OK_200({token}, callback, responses.sendRresponse)
             } else {
                 console.log("Password mismatch for user " + user._id);
-                return responses.FORBIDDEN_403(callback, response);
+                return responses.FORBIDDEN_403(callback, responses.sendRresponse);
             }
         })
         .catch(err => {
             console.log("Request failed", err);
-            return responses.INTERNAL_SERVER_ERROR_500(err, callback, response);
+            return responses.INTERNAL_SERVER_ERROR_500(err, callback, responses.sendRresponse);
         });
-
-    console.log("At the end");
 };
