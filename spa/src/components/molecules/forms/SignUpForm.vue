fe13db385c886798374aff720fa84b44e0815025
Leos Literak <leos@literak.cz>
2020-02-01 12:34:27 +0100

    personal data are hidden by default

--- spa/src/components/molecules/forms/SignUpForm.vue
+++ spa/src/components/molecules/forms/SignUpForm.vue
@@ -22,70 +22,74 @@
         <TextInput v-model="email" rules="required|email" :label="$t('sign-up.email-label')" name="email" type="email"/>
         <TextInput v-model="password" rules="required|min:6" :label="$t('sign-up.password-label')" name="password" type="password" />
         <TextInput v-model="nickname" rules="required|min:3" :label="$t('profile.nickname')" name="nickname" />
-        <TextInput v-model="drivingSince" rules="min_value:1935" :label="$t('profile.driving-since')" name="driving-since" type="number" />
 
-        <div class="sign-up-form__label">
-          <label for="vehicle">{{ $t('profile.vehicle') }}</label>
-        </div>
-        <div class="sign-up-form__input">
-          <Checkbox v-model="bike" :label="$t('profile.vehicle-bike')" name="vehicle" identifier="bike" />
-          <Checkbox v-model="car" :label="$t('profile.vehicle-car')" name="vehicle" identifier="car" />
-          <Checkbox v-model="bus" :label="$t('profile.vehicle-bus')" name="vehicle" identifier="bus" />
-          <Checkbox v-model="van" :label="$t('profile.vehicle-van')" name="vehicle" identifier="van" />
-          <Checkbox v-model="truck" :label="$t('profile.vehicle-truck')" name="vehicle" identifier="truck" />
-          <Checkbox v-model="tramway" :label="$t('profile.vehicle-tramway')" name="vehicle" identifier="tramway" />
-        </div>
+        <Checkbox v-model="personalData" :label="$t('sign-up.personal-data')" name="personal-data" identifier="personalData" />
 
-        <div class="sign-up-form__label">
-          <label for="sex">{{ $t('profile.sex') }}</label>
-        </div>
-        <div class="sign-up-form__input">
-          <Radio v-model="sex" :label="$t('profile.sex-man')" name="sex" identifier="male" />
-          <Radio v-model="sex" :label="$t('profile.sex-woman')" name="sex" identifier="female" />
-        </div>
+        <div v-if="personalData">
+          <div class="sign-up-form__label">
+            <label for="share-profile">{{ $t('profile.share-profile') }}</label>
+          </div>
+          <div class="sign-up-form__input">
+            <Radio name="share-profile" identifier="public" :label="$t('profile.public')" v-model="share" />
+            <Radio name="share-profile" identifier="private" :label="$t('profile.private')" v-model="share" />
+          </div>
 
-        <TextInput v-model="bornInYear" rules="min_value:1915" :label="$t('profile.born')" name="born-in-year" type="number" />
+          <TextInput v-model="drivingSince" rules="min_value:1935" :label="$t('profile.driving-since')" name="driving-since" type="number" />
 
-        <div class="sign-up-form__label">
-          <label for="region">{{ $t('profile.region') }}</label>
-        </div>
-        <div class="sign-up-form__input">
-          <select id="region" v-model="region">
-            <option value="">{{ $t('sign-up.region-options') }}</option>
-            <option value="PRG">Praha</option>
-            <option value="SC">Stredočeský</option>
-            <option value="JC">Jihočeský</option>
-            <option value="PLS">Plzeňský</option>
-            <option value="KV">Karlovarský</option>
-            <option value="UST">Ústecký</option>
-            <option value="LBR">Liberecký</option>
-            <option value="KH">Královohradecký</option>
-            <option value="PRD">Pardubický</option>
-            <option value="VSC">Vysočina</option>
-            <option value="JM">Jihomoravský</option>
-            <option value="OLM">Olomoucký</option>
-            <option value="ZLN">Zlínský</option>
-            <option value="MS">Moravskoslezský</option>
-          </select>
-        </div>
+          <div class="sign-up-form__label">
+            <label for="vehicle">{{ $t('profile.vehicle') }}</label>
+          </div>
+          <div class="sign-up-form__input">
+            <Checkbox v-model="bike" :label="$t('profile.vehicle-bike')" name="vehicle" identifier="bike" />
+            <Checkbox v-model="car" :label="$t('profile.vehicle-car')" name="vehicle" identifier="car" />
+            <Checkbox v-model="bus" :label="$t('profile.vehicle-bus')" name="vehicle" identifier="bus" />
+            <Checkbox v-model="van" :label="$t('profile.vehicle-van')" name="vehicle" identifier="van" />
+            <Checkbox v-model="truck" :label="$t('profile.vehicle-truck')" name="vehicle" identifier="truck" />
+            <Checkbox v-model="tramway" :label="$t('profile.vehicle-tramway')" name="vehicle" identifier="tramway" />
+          </div>
 
-        <div class="sign-up-form__label">
-          <label for="education">{{ $t('profile.education') }}</label>
-        </div>
-        <div class="sign-up-form__input">
-          <Radio v-model="education" :label="$t('profile.edu-basic')" name="education" identifier="primary" />
-          <Radio v-model="education" :label="$t('profile.edu-high')" name="education" identifier="secondary" />
-          <Radio v-model="education" :label="$t('profile.edu-university')" name="education" identifier="university" />
-        </div>
+          <div class="sign-up-form__label">
+            <label for="sex">{{ $t('profile.sex') }}</label>
+          </div>
+          <div class="sign-up-form__input">
+            <Radio v-model="sex" :label="$t('profile.sex-man')" name="sex" identifier="male" />
+            <Radio v-model="sex" :label="$t('profile.sex-woman')" name="sex" identifier="female" />
+          </div>
 
-        <div class="sign-up-form__label">
-          <label for="share-profile">{{ $t('profile.share-profile') }}</label>
-        </div>
-        <div class="sign-up-form__input">
-          <Radio name="share-profile" identifier="public" :label="$t('profile.public')" v-model="share" />
-          <Radio name="share-profile" identifier="private" :label="$t('profile.private')" v-model="share" />
-        </div>
+          <TextInput v-model="bornInYear" rules="min_value:1915" :label="$t('profile.born')" name="born-in-year" type="number" />
 
+          <div class="sign-up-form__label">
+            <label for="region">{{ $t('profile.region') }}</label>
+          </div>
+          <div class="sign-up-form__input">
+            <select id="region" v-model="region">
+              <option value="">{{ $t('sign-up.region-options') }}</option>
+              <option value="PRG">Praha</option>
+              <option value="SC">Stredočeský</option>
+              <option value="JC">Jihočeský</option>
+              <option value="PLS">Plzeňský</option>
+              <option value="KV">Karlovarský</option>
+              <option value="UST">Ústecký</option>
+              <option value="LBR">Liberecký</option>
+              <option value="KH">Královohradecký</option>
+              <option value="PRD">Pardubický</option>
+              <option value="VSC">Vysočina</option>
+              <option value="JM">Jihomoravský</option>
+              <option value="OLM">Olomoucký</option>
+              <option value="ZLN">Zlínský</option>
+              <option value="MS">Moravskoslezský</option>
+            </select>
+          </div>
+
+          <div class="sign-up-form__label">
+            <label for="education">{{ $t('profile.education') }}</label>
+          </div>
+          <div class="sign-up-form__input">
+            <Radio v-model="education" :label="$t('profile.edu-basic')" name="education" identifier="primary" />
+            <Radio v-model="education" :label="$t('profile.edu-high')" name="education" identifier="secondary" />
+            <Radio v-model="education" :label="$t('profile.edu-university')" name="education" identifier="university" />
+          </div>
+        </div>
 
         <Checkbox v-model="termsAndConditions"  :rules="{ required: { allowFalse: false} }" :label="$t('sign-up.terms-label')" name="terms-and-conditions" identifier="termsAndConditions" />
         <Checkbox v-model="personalDataProcessing"  :rules="{ required: { allowFalse: false} }" :label="$t('sign-up.processing-label')" name="personal-data-processing" identifier="personalDataProcessing" />
@@ -155,6 +159,7 @@ export default {
     region: '',
     education: '',
     share: null,
+    personalData: false,
     success: null,
     signingIn: false,
   }),
@@ -162,7 +167,6 @@ export default {
     async submitForm() {
       this.signingIn = true;
       this.errors = [];
-      // validateForm.call(this);
       if (this.errors.length > 0) {
         this.signingIn = false;
         return false;
@@ -178,6 +182,10 @@ export default {
           dataProcessing: this.personalDataProcessing,
           emails: this.emailNotifications,
         });
+
+        if (!this.personalData) {
+          return true;
+        }
 
         if (data.token !== undefined) {
           const jwtData = jwtDecode(data.token);
@@ -220,9 +228,6 @@ export default {
         }
       }
       return this.errors.length !== 0;
-    },
-    checkClick() {
-      console.log('aaaaaaaaaaaa');
     },
   },
 };
