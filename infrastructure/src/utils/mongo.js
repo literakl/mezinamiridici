9c068ea59435c258f8206cf389cbf2d65ecc50b9
Leos Literak <leos@literak.cz>
2020-04-05 20:12:01 +0200

    fixed scenario when user has not voted yet

--- infrastructure/src/utils/mongo.js
+++ infrastructure/src/utils/mongo.js
@@ -95,10 +95,11 @@ async function getPoll(dbClient, pipeline) {
     item.votes = item.poll[0].votes;
     item.votes.total = item.votes.neutral + item.votes.trivial + item.votes.dislike + item.votes.hate;
     delete item.poll;
-    if (item.me) {
+    console.log(item);
+    if (item.me[0]) {
         item.my_vote = item.me[0].vote;
-        delete item.me;
     }
+    delete item.me;
     return item;
 }
 
