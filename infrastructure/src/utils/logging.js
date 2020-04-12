c59ec89567689731435656557746ccdb40bd5f3b
Leos Literak <leos@literak.cz>
2020-04-12 10:36:42 +0200

    fixed log level; added custom log for tests

--- infrastructure/src/utils/logging.js
+++ infrastructure/src/utils/logging.js
@@ -1,23 +1,38 @@
 const { createLogger, format, transports } = require('winston');
 const { combine, timestamp, label, prettyPrint } = format;
 
-const logger = createLogger({
-    level: 'info',
-    format: combine(
-        timestamp(),
-        prettyPrint()
-    ),
-    // defaultMeta: { service: 'user-service' },
-    transports: [
-        new transports.File({ filename: 'application.log' }),
-        new transports.File({ filename: 'error.log', level: 'warn', handleExceptions: true, exitOnError: false })
-    ]
-});
+let logger;
 
-if (process.env.NODE_ENV !== 'production') {
-    logger.add(new transports.Console({
-        format: format.simple()
-    }));
+if (process.env.NODE_ENV === 'test') {
+    logger = createLogger({
+        level: 'debug',
+        format: combine(
+            timestamp(),
+            prettyPrint()
+        ),
+        transports: [
+            new transports.File({ filename: 'test.log' }),
+        ]
+    });
+} else {
+    logger = createLogger({
+        level: 'debug',
+        format: combine(
+            timestamp(),
+            prettyPrint()
+        ),
+        // defaultMeta: { service: 'user-service' },
+        transports: [
+            new transports.File({ filename: 'application.log' }),
+            new transports.File({ filename: 'error.log', level: 'warn', handleExceptions: true, exitOnError: false })
+        ]
+    });
+
+    if (process.env.NODE_ENV !== 'production') {
+        logger.add(new transports.Console({
+            format: format.simple()
+        }));
+    }
 }
 
 logger.on('finish', function (info) {
--- infrastructure/test/api.int.test.js
+++ infrastructure/test/api.int.test.js
@@ -1,22 +1,23 @@
 const axios = require('axios').default;
-const dotenv = require('dotenv');
-const app = require('../src/server.js');
-dotenv.config({ path: 'C:\\dev\\mezinamiridici\\infrastructure\\.test.env' });
-
 axios.defaults.headers.post['Content-Type'] = 'application/json; charset=utf-8';
 axios.defaults.headers.patch['Content-Type'] = 'application/json; charset=utf-8';
+const dotenv = require('dotenv');
+dotenv.config({ path: 'C:\\dev\\mezinamiridici\\infrastructure\\.test.env' });
+const logger = require("../src/utils/logging");
+const app = require('../src/server.js');
 
 test('end to end test', async () => {
     const polls = await axios.get(`http://localhost:3000/bff/polls/last`);
+    console.log(polls.data);
     expect(polls.data).toBeDefined();
 });
 
 beforeAll(() => {
     app.listen(3000, '0.0.0.0')
-        .then(r => console.log("Server started"));
+        .then(r => logger.info("Server started"));
 });
 
 afterAll(() => {
     if (app.close())
-        console.log("Server stopped");
+        logger.info("Server stopped");
 });
