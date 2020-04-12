7f624c36b0303863dd30a0e65681b7c389a31320
Leos Literak <leos@literak.cz>
2020-04-12 09:58:02 +0200

    add await

--- infrastructure/test/api.int.test.js
+++ infrastructure/test/api.int.test.js
@@ -6,13 +6,9 @@ dotenv.config({ path: 'C:\\dev\\mezinamiridici\\infrastructure\\.test.env' });
 axios.defaults.headers.post['Content-Type'] = 'application/json; charset=utf-8';
 axios.defaults.headers.patch['Content-Type'] = 'application/json; charset=utf-8';
 
-function sum(a, b) {
-    return a * b;
-}
-
-test('end to end test', () => {
-    const polls = axios.get(`http://127.0.0.1:3000/bff//polls/last`);
-    expect(polls).toBeFalsy();
+test('end to end test', async () => {
+    const polls = await axios.get(`http://localhost:3000/bff/polls/last`);
+    expect(polls.data).toBeDefined();
 });
 
 beforeAll(() => {
