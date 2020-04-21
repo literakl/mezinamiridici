b5f84ad3f818b0dd5de85e65483ff9959545201a
Leos Literak <leos@literak.cz>
2020-04-21 20:12:04 +0200

    initialize mongo indexes

--- infrastructure/src/utils/mongo_setup.js
+++ infrastructure/src/utils/mongo_setup.js
@@ -1,3 +1,6 @@
+conn = new Mongo();
+db = conn.getDB("rm");
+
 db.users.createIndex( { "auth.email": 1 }, { collation: {locale: "cs", strength: 2}, unique: true } );
 db.users.createIndex( { "bio.nickname": 1 }, { collation: {locale: "cs", strength: 1}, unique: true } );
 db.items.createIndex( { "info.date": 1 } );
