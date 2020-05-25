62c5d6aff33061bff0a1522895692b586a6c9edf
Leos Literak <leos@literak.cz>
2020-05-25 08:43:53 +0200

    bug fix #44 - align icon and links

--- spa/src/App.vue
+++ spa/src/App.vue
@@ -16,11 +16,11 @@
             </div>
           </b-navbar-nav>
 
-        <b-navbar-nav class="ml-auto" align-v="center">
+        <b-navbar-nav class="ml-auto align-items-center">
           <b-nav-item :to="{ name: 'sign-in'}" v-if="!authorized">{{ $t('app.sign-in-up') }}</b-nav-item>
             <b-nav-item-dropdown v-if="authorized" toggle-class="text-warning" right>
               <template v-slot:button-content>
-                <b-icon-person-fill font-scale="2"></b-icon-person-fill> {{ nickname }}
+                <b-icon-person-fill scale="2"></b-icon-person-fill> {{ nickname }}
               </template>
               <b-dropdown-item :to="{ name: 'user-profile', params: { id: userId } }" v-if="authorized">{{ $t('app.my-profile') }}</b-dropdown-item>
               <b-dropdown-item :to="{ name: 'update-password'}">{{ $t('app.change-password') }}</b-dropdown-item>
@@ -28,7 +28,7 @@
             </b-nav-item-dropdown>
             <b-nav-item-dropdown toggle-class="text-warning" right>
               <template v-slot:button-content>
-                <b-icon-info font-scale="2"></b-icon-info>
+                <b-icon-info scale="2"></b-icon-info>
               </template>
               <b-dropdown-item :to="{ name: 'help'}">{{ $t('app.help') }}</b-dropdown-item>
               <b-dropdown-item :to="{ name: 'mission'}">{{ $t('app.our-mission') }}</b-dropdown-item>
