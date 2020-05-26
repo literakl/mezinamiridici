fefb820a8d5b14505c1784289d2c0443276f3986
Leos Literak <leos@literak.cz>
2020-05-26 22:44:27 +0200

    fix the alignment

--- spa/src/components/molecules/SeriesForm.vue
+++ spa/src/components/molecules/SeriesForm.vue
@@ -20,20 +20,20 @@
         </b-input-group>
       </b-form-group>
       <b-form-group label="Sex:" label-cols-sm="2">
-        <b-form-checkbox-group id="sex" v-model="group.sex" name="sex">
+        <b-form-checkbox-group id="sex" v-model="group.sex" name="sex" class="align-nicely mt-2">
           <b-form-checkbox value="men">Men</b-form-checkbox>
           <b-form-checkbox value="women">Women</b-form-checkbox>
         </b-form-checkbox-group>
       </b-form-group>
-      <b-form-group label="Education:" label-cols-sm="2">
-        <b-form-checkbox-group id="edu" v-model="group.education" name="edu">
+      <b-form-group label="Education:" label-cols-sm="2" class="pb-0">
+        <b-form-checkbox-group id="edu" v-model="group.education" name="edu" class="align-nicely mt-2">
           <b-form-checkbox value="primary">Primary</b-form-checkbox>
           <b-form-checkbox value="secondary">Secondary</b-form-checkbox>
           <b-form-checkbox value="university">University</b-form-checkbox>
         </b-form-checkbox-group>
       </b-form-group>
       <b-form-group label="Vehicles:" label-cols-sm="2">
-        <b-form-checkbox-group id="vehicle" v-model="group.vehicles" name="vehicle">
+        <b-form-checkbox-group id="vehicle" v-model="group.vehicles" name="vehicle" class="align-nicely mt-2">
           <b-form-checkbox value="bike">Bike</b-form-checkbox>
           <b-form-checkbox value="bus">Bus</b-form-checkbox>
           <b-form-checkbox value="car">Car</b-form-checkbox>
@@ -42,7 +42,7 @@
         </b-form-checkbox-group>
       </b-form-group>
       <b-form-group label="Regions:" label-cols-sm="2">
-        <b-form-checkbox-group id="region" v-model="group.regions" name="region">
+        <b-form-checkbox-group id="region" v-model="group.regions" name="region" class="align-nicely mt-2">
           <b-form-checkbox value="PRG">{{ $t('profile.regions.PRG') }}</b-form-checkbox>
           <b-form-checkbox value="SC">{{ $t('profile.regions.SC') }}</b-form-checkbox>
           <b-form-checkbox value="JC">{{ $t('profile.regions.JC') }}</b-form-checkbox>
@@ -72,3 +72,12 @@ export default {
   methods: {},
 };
 </script>
+
+<style>
+  .align-nicely {
+    display: grid;
+    grid-column-gap: 10px;
+    grid-row-gap: 10px;
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+</style>
