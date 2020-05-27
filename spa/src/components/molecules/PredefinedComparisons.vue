09a9d650810379dca343f9e75016263d1d97ce49
Leos Literak <leos@literak.cz>
2020-05-27 19:02:02 +0200

    hash does not work, page does not scroll there

--- spa/src/components/molecules/PredefinedComparisons.vue
+++ spa/src/components/molecules/PredefinedComparisons.vue
@@ -1,22 +1,22 @@
 <template>
   <div id="scroll">
     {{ $t('poll.analysis.comparison') }}
-    <router-link :to="{ name: 'analyse-poll', params: { slug, type: 'muzi_zeny' }, hash: '#scroll' }">
+    <router-link :to="{ name: 'analyse-poll', params: { slug, type: 'muzi_zeny' } }">
       {{ $t('poll.analysis.men_women') }}
     </router-link>,
-    <router-link :to="{ name: 'analyse-poll', params: { slug, type: 'auto_kamion' }, hash: '#scroll' }">
+    <router-link :to="{ name: 'analyse-poll', params: { slug, type: 'auto_kamion' } }">
       {{ $t('poll.analysis.car_truck') }}
     </router-link>,
-    <router-link :to="{ name: 'analyse-poll', params: { slug, type: 'auto_motorka' }, hash: '#scroll' }">
+    <router-link :to="{ name: 'analyse-poll', params: { slug, type: 'auto_motorka' } }">
       {{ $t('poll.analysis.car_bike') }}
     </router-link>,
-    <router-link :to="{ name: 'analyse-poll', params: { slug, type: 'zajic_zkuseny' }, hash: '#scroll' }">
+    <router-link :to="{ name: 'analyse-poll', params: { slug, type: 'zajic_zkuseny' } }">
       {{ $t('poll.analysis.unseasoned_veteran') }}
     </router-link>,
-    <router-link :to="{ name: 'analyse-poll', params: { slug, type: 'praha_brno' }, hash: '#scroll' }">
+    <router-link :to="{ name: 'analyse-poll', params: { slug, type: 'praha_brno' } }">
       {{ $t('poll.analysis.praha_brno') }}
     </router-link>,
-    <router-link :to="{ name: 'analyse-poll', params: { slug, type: 'vlastni' }, hash: '#scroll' }">
+    <router-link :to="{ name: 'analyse-poll', params: { slug, type: 'vlastni' } }">
       {{ $t('poll.analysis.custom') }}
     </router-link>
   </div>
