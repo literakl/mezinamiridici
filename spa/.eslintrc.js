d718355d1cf2d48fdf0d71a52b0988671cd9f60c
Leos Literak <leos@literak.cz>
2020-06-14 18:01:53 +0200

    ignore eslint error when working with parameter's  properties

--- spa/.eslintrc.js
+++ spa/.eslintrc.js
@@ -12,11 +12,12 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'max-len': [0, 190, 4],
-    'linebreak-style': [0, "windows"],
+    'linebreak-style': [0, 'windows'],
     'no-underscore-dangle': 'off',
     'one-var-declaration-per-line': 'off',
     'one-var': 'off',
-    "no-use-before-define": ["error", { "functions": false }]
+    'no-use-before-define': ['error', { "functions": false }],
+    'no-param-reassign': ['error', { "props": false }]
   },
   parserOptions: {
     parser: 'babel-eslint',
