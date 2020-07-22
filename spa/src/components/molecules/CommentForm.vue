cb0c3615dc63e884e17c26093c53db83ff12e71e
kotte-sanya <SanyaAlexandrovich2020@yandex.com>
2020-07-23 04:32:29 +0900

    #57 emoji issue fix again.

--- spa/src/components/molecules/CommentForm.vue
+++ spa/src/components/molecules/CommentForm.vue
@@ -7,6 +7,16 @@
       {{ $t('generic.internal-error') }}
     </b-alert>
     <Button :disabled="sending" class="mt-2" size="sm" :value="$t('comment.send-button')" @clicked="send"/>
+
+      <b-button :id="`emoji_list_${commentId}`" class="mt-2" variant="outline" size="sm">
+        &#x1F600;
+      </b-button>
+
+      <b-popover :target="`emoji_list_${commentId}`" triggers="hover" placement="bottom">
+        <b-button v-for="(emoji, index) in emojiArray" v-bind:key="index" v-on:click="addEmoji(index)"
+        variant="outline" size="sm">{{emoji}}</b-button>
+      </b-popover>
+
   </div>
 </template>
 
@@ -22,14 +32,20 @@ export default {
       type: Boolean,
       default: true,
     },
+    commentId: String,
   },
   components: {
     Button,
   },
   data: () => ({
-    text: null,
+    text: '',
     sending: null,
     error: null,
+    emojiArray: ['\u{1F600}', '\u{1F603}', '\u{1F601}', '\u{1F606}',
+      '\u{1F60B}', '\u{1F61B}', '\u{1F61C}', '\u{1F92D}', '\u{1F92B}',
+      '\u{1F910}', '\u{1F928}', '\u{1F644}', '\u{1F614}', '\u{1F634}',
+      '\u{1F637}', '\u{1F975}', '\u{1F60E}', '\u{2639}', '\u{1F633}',
+      '\u{1F62D}', '\u{1F629}', '\u{1F621}', '\u{1F620}', '\u{1F47F}'],
   }),
   methods: {
     dismiss() {
@@ -57,6 +73,9 @@ export default {
         this.error = true;
       }
       this.sending = false;
+    },
+    addEmoji(idx) {
+      this.text += this.emojiArray[idx];
     },
   },
 };
