8db34eb82e70cf21534ad5ae1be9abbe2076b9d5
Leos Literak <leos@literak.cz>
2020-08-12 08:04:13 +0200

    remove comments

--- infrastructure/src/utils/mailService.js
+++ infrastructure/src/utils/mailService.js
@@ -10,12 +10,14 @@ const COMPILED_TEMPLATES = {};
 let transporter;
 switch (process.env.MAILER) {
   case 'SES':
-    transporter = createAWSSESTransporter();
+    createAWSSESTransporter();
+    logger.info('AWS SES transport');
     break;
   case 'SMTP':
   case 'FAKE':
   default:
-    createFakeTransporter().then((x) => { transporter = x; });
+    createFakeTransporter();
+    logger.info('Fake SMTP transport');
 }
 
 async function sendEmail(config, options, context) {
@@ -53,18 +55,18 @@ function processTemplate(templateName, filename, context) {
   return compiled(context);
 }
 
-async function createFakeTransporter() {
-  const testAccount = await nodemailer.createTestAccount();
-
-  // create reusable transporter object using the default SMTP transport
-  return nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: testAccount.user, // generated ethereal user
-      pass: testAccount.pass, // generated ethereal password
-    },
+// create reusable transporter object using the default SMTP transport
+function createFakeTransporter() {
+  nodemailer.createTestAccount().then((testAccount) => {
+    transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: testAccount.user, // generated ethereal user
+        pass: testAccount.pass, // generated ethereal password
+      },
+    });
   });
 }
 
@@ -72,7 +74,7 @@ function createAWSSESTransporter() {
   // eslint-disable-next-line global-require
   const AWS = require('aws-sdk');
   AWS.config.region = process.env.AWS_REGION;
-  return nodemailer.createTransport({
+  transporter = nodemailer.createTransport({
     SES: new AWS.SES({
       apiVersion: '2010-12-01',
     }),
