0b707d0b5ef666770bb6e1f5ff9f7c01cb4c772f
Leos Literak <leos@literak.cz>
2020-08-21 21:56:37 +0200

    #80 - code review for select picture

--- infrastructure/src/handlers/items/getItemPictures.js
+++ infrastructure/src/handlers/items/getItemPictures.js
@@ -1,12 +1,13 @@
+const fs = require('fs');
 const api = require('../../utils/api.js');
 const auth = require('../../utils/authenticate');
 const logger = require('../../utils/logging');
-const fs = require('fs');
 
-const imagePath = process.env.STREAM_PICTURES_DIR;
-const returnUrl = process.env.STREAM_PICTURES_PATH;
-const defaultFileName = process.env.STREAM_PICTURES_DEFAULT;
-const thumbPrefix = 'thumb';
+const IMAGES_DIR = process.env.STREAM_PICTURES_DIR;
+const WEB_PATH = process.env.STREAM_PICTURES_PATH;
+const DEFAULT_IMAGE = process.env.STREAM_PICTURES_DEFAULT;
+const THUMBNAIL_PREFIX = 'thumb_';
+const EXTENSIONS = ['jpg', 'jpeg', 'png', 'gif', 'webp'];
 
 module.exports = (app) => {
   app.options('/v1/items/pictures', auth.cors);
@@ -14,55 +15,49 @@ module.exports = (app) => {
   app.get('/v1/items/pictures', auth.required, async (req, res) => {
     logger.verbose('get Item Pictures handler starts');
     try {
+      if (!IMAGES_DIR || !WEB_PATH || !DEFAULT_IMAGE) {
+        return api.sendInternalError(res, api.createError('Incomplete configuration', 'sign-in.something-went-wrong'));
+      }
 
-      if( !imagePath || !returnUrl || !defaultFileName ) throw 'Missed environment variable.';
+      const files = [];
+      const result = [];
 
-      const fileNameList = [];
-      const returnList = [];
-
-      fs.readdirSync(imagePath).forEach(file => {
-        if(checkExtension(file)) fileNameList.push(file);
+      fs.readdirSync(IMAGES_DIR).forEach((file) => {
+        if (isPicture(file)) {
+          files.push(file);
+        }
       });
 
-      if(!isExistFile(defaultFileName, fileNameList)) throw 'there no default file';
-      if(!isExistThumb(defaultFileName, fileNameList)) throw 'there no thumbnail of default file.';
-
-      fileNameList.forEach((item) => {
-        if(!isThumbName(item, fileNameList) && isExistThumb(item, fileNameList)) returnList.push(getItemBody(item, item === defaultFileName));
+      files.forEach((item) => {
+        if (checkThumbnail(item, files)) {
+          result.push(getItemBody(item, item === DEFAULT_IMAGE));
+        }
       });
 
-      logger.info('get Item Pictures handler end');
-      return api.sendCreated(res, api.createResponse(returnList));
+      return api.sendCreated(res, api.createResponse(result));
     } catch (err) {
       logger.error('Request failed', err);
-      return api.sendInternalError(res, api.createError(err, 'sign-in.something-went-wrong'));
+      return api.sendInternalError(res, api.createError('Internal error', 'sign-in.something-went-wrong'));
     }
   });
-
-  
 };
 
-const checkExtension = (fileName) => {
-  const extensionList = ['jpg', 'jpeg', 'png', 'gif', 'webp'];
+function isPicture(fileName) {
   const extension = fileName.split('.').pop();
-  if(extensionList.indexOf(extension.toLocaleLowerCase()) === -1) return false;
-  return true;
+  return EXTENSIONS.indexOf(extension.toLocaleLowerCase()) !== -1;
 }
 
-const getItemBody = (fileName, isDefault) => {
+function getItemBody(fileName, isDefault) {
   return {
-    thumbnail: `${returnUrl}/${thumbPrefix}_${fileName}`,
-    path: `${returnUrl}/${fileName}`,
+    path: `${WEB_PATH}/${fileName}`,
+    thumbnail: `${WEB_PATH}/${THUMBNAIL_PREFIX}${fileName}`,
     default_picture: isDefault,
-  }
+  };
 }
 
-const isThumbName = (fileName) => {
-  return thumbPrefix === fileName.split('_').shift();
-}
-const isExistFile = (fileName, list) => {
-  return list.indexOf(fileName) > -1;
-}
-const isExistThumb = (fileName, list) => {
-  return list.indexOf(`${thumbPrefix}_${fileName}`) > -1;
+function checkThumbnail(file, files) {
+  if (file.startsWith(THUMBNAIL_PREFIX)) {
+    return false;
+  }
+  return files.indexOf(`${THUMBNAIL_PREFIX}${file}`) > -1;
 }
