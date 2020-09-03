e42feb22498f2154a6b7c42f0e2a538aad7d483c
Leos Literak <leos@literak.cz>
2020-09-03 20:58:43 +0200

    #87 - eslint issues

--- infrastructure/src/jobs/parseAccidents.js
+++ infrastructure/src/jobs/parseAccidents.js
@@ -5,27 +5,23 @@ const slugify = require('slugify');
 const dayjs = require('dayjs');
 const customParseFormat = require('dayjs/plugin/customParseFormat');
 const weekOfYear = require('dayjs/plugin/weekOfYear');
-const sanitizeHtml = require('sanitize-html');
-
-dayjs.extend(customParseFormat);
-dayjs.extend(weekOfYear);
-
+const cron = require('node-cron');
+const cheerio = require('cheerio');
+const cheerioAdv = require('cheerio-advanced-selectors');
+const FormData = require('form-data');
 const edjsHTML = require('editorjs-html');
 
 const mongo = require('../utils/mongo.js');
 const api = require('../utils/api.js');
 
-const cron = require('node-cron');
-const cheerio = require('cheerio');
-const cheerioAdv = require('cheerio-advanced-selectors');
-const FormData = require('form-data');
-const mongo = require('../utils/mongo.js');
+dayjs.extend(customParseFormat);
+dayjs.extend(weekOfYear);
 
 const edjsParser = edjsHTML(api.edjsHtmlCustomParser());
 
 const { combine, printf } = format;
 const myFormat = printf(info => `${info.timestamp} [${info.level}]: ${info.message === Object(info.message) ? stringify(info.message) : info.message}`);
-const appendTimestamp = format((info, opts) => {
+const appendTimestamp = format((info) => {
   info.timestamp = dayjs().format('YYYY-MM-DD HH:mm:ss:SSS');
   return info;
 });
@@ -54,33 +50,33 @@ logger.stream = {
 
 const cheerioParser = cheerioAdv.wrap(cheerio);
 
-const ACCIDENTS_RECORDS_URL = process.env.ACCIDENTS_RECORDS_URL;
-const ACCIDENTS_RECORDS_CRON = process.env.ACCIDENTS_RECORDS_CRON;
-const ACCIDENTS_RECORDS_RETRY = process.env.ACCIDENTS_RECORDS_RETRY;
+const { ACCIDENTS_RECORDS_URL } = process.env;
+const { ACCIDENTS_RECORDS_CRON } = process.env;
+const { ACCIDENTS_RECORDS_RETRY } = process.env;
 const dateFormatType = 'DD.MM.YYYY';
 
 const keyArray = [
-  'region', 'counts', 'deaths', 'severely', 'slighty', 'damage', 'speed', 'priority', 'passing', 'driving', 'drunk', 'other'
-]
+  'region', 'counts', 'deaths', 'severely', 'slighty', 'damage', 'speed', 'priority', 'passing', 'driving', 'drunk', 'other',
+];
 let formDataBody = {};
 
 let currentScheduleExpression = ACCIDENTS_RECORDS_CRON;
 
 async function setInitFormData() {
   logger.debug('::: get content when first loading');
-  let html = await getAxios(ACCIDENTS_RECORDS_URL);
-  let $ = await cheerioParser.load(html);
+  const html = await getAxios(ACCIDENTS_RECORDS_URL);
+  const $ = await cheerioParser.load(html);
   formDataBody = {
-      'ctl00_Application_ScriptManager1_HiddenField' : $('#ctl00_Application_ScriptManager1_HiddenField').val(),
-      '__EVENTTARGET' : $('#__EVENTTARGET').val(),
-      '__EVENTARGUMENT' : $('#__EVENTARGUMENT').val(),
-      '__VIEWSTATE' : $('#__VIEWSTATE').val(),
-      '__VIEWSTATEGENERATOR' : $('#__VIEWSTATEGENERATOR').val(),
-      '__EVENTVALIDATION' : $('#__EVENTVALIDATION').val(),
-      'ctl00$Application$ddlKraje' : 'Česká republika',
-      // 'ctl00$Application$txtDatum' : date,
-      'ctl00$Application$cmdZobraz' : 'Zobrazit',
-  }
+    ctl00_Application_ScriptManager1_HiddenField: $('#ctl00_Application_ScriptManager1_HiddenField').val(),
+    __EVENTTARGET: $('#__EVENTTARGET').val(),
+    __EVENTARGUMENT: $('#__EVENTARGUMENT').val(),
+    __VIEWSTATE: $('#__VIEWSTATE').val(),
+    __VIEWSTATEGENERATOR: $('#__VIEWSTATEGENERATOR').val(),
+    __EVENTVALIDATION: $('#__EVENTVALIDATION').val(),
+    ctl00$Application$ddlKraje: 'Česká republika',
+    // 'ctl00$Application$txtDatum' : date,
+    ctl00$Application$cmdZobraz: 'Zobrazit',
+  };
 }
 
 async function parseData(dbClient, date) {
@@ -89,10 +85,10 @@ async function parseData(dbClient, date) {
   logger.debug('::: require date');
   logger.debug(date);
 
-  formDataBody['ctl00$Application$txtDatum'] = date;
+  formDataBody.ctl00$Application$txtDatum = date;
   const formData = new FormData();
-  for(key in formDataBody){
-    formData.append(key, (formDataBody[key]) ? formDataBody[key]: '');
+  for (const key in formDataBody) {
+    formData.append(key, (formDataBody[key]) ? formDataBody[key] : '');
   }
 
   const html = await getAxios(ACCIDENTS_RECORDS_URL, formData);
@@ -103,7 +99,7 @@ async function parseData(dbClient, date) {
   const regionsData = $('#celacr tr');
   let result = false;
 
-  if (regionsData && regionsData.length > 0){
+  if (regionsData && regionsData.length > 0) {
     result = await treatData(dbClient, regionsData, date, $);
   } else {
     result = false;
@@ -114,7 +110,6 @@ async function parseData(dbClient, date) {
 }
 
 async function scheduleParsing() {
-
   const task = cron.schedule(currentScheduleExpression, async () => {
     const dbClient = await mongo.connectToDatabase();
     logger.debug('Mongo connected');
@@ -212,10 +207,7 @@ async function treatData(dbClient, region, date, $) {
 }
 
 async function getBlog(dbClient) {
-  const keys = [
-    'deaths', 'severely', 'slighty'
-  ]
-
+  const keys = ['deaths', 'severely', 'slighty'];
   const yesterday = dayjs().subtract(1, 'day').toDate();
   const prevYearDate = dayjs().subtract(1, 'day').subtract(1, 'year').toDate();
   const prevYearStartDate = new Date(prevYearDate.getFullYear(), 0, 1);
@@ -225,11 +217,11 @@ async function getBlog(dbClient) {
   const thisYearData = await storeDataByDateRange(dbClient, thisYearStartDate, yesterday);
 
   const compareData = {
-    'day': { prev: {}, now: {} },
-    'week': { prev: {}, now: {} },
-    'month': { prev: {}, now: {} },
-    'year': { prev: {}, now: {} }
-  }
+    day: { prev: {}, now: {} },
+    week: { prev: {}, now: {} },
+    month: { prev: {}, now: {} },
+    year: { prev: {}, now: {} },
+  };
   initCompareData(keys, compareData);
   makeCompareData(keys, prevYearData, prevYearDate, compareData, true);
   makeCompareData(keys, thisYearData, yesterday, compareData, false);
@@ -238,42 +230,42 @@ async function getBlog(dbClient) {
   return compareData;
 }
 
-function makeCompareData(keys, allYearData, compareDate, resultData, isPrev){
+function makeCompareData(keys, allYearData, compareDate, resultData, isPrev) {
   allYearData.forEach((item) => {
     const date = new Date(item.date);
     if (date.toLocaleDateString() === compareDate.toLocaleDateString()) {
       if (isPrev) {
-        keys.map((k) => {resultData.day.prev[k] = item.total.impact[k]});
+        keys.map((k) => { resultData.day.prev[k] = item.total.impact[k]; });
       } else {
-        keys.map((k) => {resultData.day.now[k] = item.total.impact[k]});
+        keys.map((k) => { resultData.day.now[k] = item.total.impact[k]; });
       }
     }
     if (dayjs(date).week() === dayjs(compareDate).week()) {
       if (isPrev) {
-        keys.map((k) => {resultData.week.prev[k] += item.total.impact[k]});
+        keys.map((k) => { resultData.week.prev[k] += item.total.impact[k]; });
       } else {
-        keys.map((k) => {resultData.week.now[k] += item.total.impact[k]});
+        keys.map((k) => { resultData.week.now[k] += item.total.impact[k]; });
       }
     }
     if (date.getMonth() === compareDate.getMonth()) {
       if (isPrev) {
-        keys.map((k) => {resultData.month.prev[k] += item.total.impact[k]});
+        keys.map((k) => { resultData.month.prev[k] += item.total.impact[k]; });
       } else {
-        keys.map((k) => {resultData.month.now[k] += item.total.impact[k]});
+        keys.map((k) => { resultData.month.now[k] += item.total.impact[k]; });
       }
     }
     if (isPrev) {
-      keys.map((k) => {resultData.year.prev[k] += item.total.impact[k]});
+      keys.map((k) => { resultData.year.prev[k] += item.total.impact[k]; });
     } else {
-      keys.map((k) => {resultData.year.now[k] += item.total.impact[k]});
+      keys.map((k) => { resultData.year.now[k] += item.total.impact[k]; });
     }
-  })
+  });
 }
 
 function initCompareData(keys, obj) {
-  for (v1 in obj) {
-    for (v2 in obj[v1]) {
-      keys.map( (i) => { obj[v1][v2][i] = 0 } );
+  for (const v1 in obj) {
+    for (const v2 in obj[v1]) {
+      keys.map((i) => { obj[v1][v2][i] = 0; });
     }
   }
 }
@@ -284,14 +276,12 @@ async function storeDataByDateRange(dbClient, start, end) {
   const endDate = new Date(end);
 
   let loop = new Date(startDate);
-  while(loop <= endDate){
+  while (loop <= endDate) {
     const data = await getStoredData(dbClient, dayjs(loop).format('YYYY-MM-DD'));
     if (data.length > 0) {
       resultData.push(...data);
-    } else {
-      if (await parseData(dbClient, dayjs(loop).format(dateFormatType))) {
-        resultData.push(...await getStoredData(dbClient, dayjs(loop).format('YYYY-MM-DD')));
-      };
+    } else if (await parseData(dbClient, dayjs(loop).format(dateFormatType))) {
+      resultData.push(...await getStoredData(dbClient, dayjs(loop).format('YYYY-MM-DD')));
     }
     const newDate = loop.setDate(loop.getDate() + 1);
     loop = new Date(newDate);
@@ -300,27 +290,26 @@ async function storeDataByDateRange(dbClient, start, end) {
 }
 
 async function getStoredData(dbClient, date) {
-  return await dbClient.db().collection('accidents').find({ date: new Date(date) }).toArray();
+  return dbClient.db().collection('accidents').find({ date: new Date(date) }).toArray();
 }
 
 async function storeBlogItem(dbClient, data) {
-  const blogId = mongo.generateTimeId();
   const publishDate = dayjs().subtract(1, 'day');
   const title = `A States for ${dayjs(publishDate).format(dateFormatType)}`;
-  const picture = process.env.STREAM_PICTURES_PATH + '/' + process.env.STREAM_PICTURES_DEFAULT;
+  const picture = `${process.env.STREAM_PICTURES_PATH}/${process.env.STREAM_PICTURES_DEFAULT}`;
   const blogAuthor = await mongo.getIdentity(dbClient, process.env.COMPARE_BLOG_AUTHOR);
-  const source = {date: new Date().getTime(), blocks:[], version: '2.18.0'};
+  const source = { date: new Date().getTime(), blocks: [], version: '2.18.0' };
 
   const lastYearDate = publishDate.subtract(1, 'year');
   const week = publishDate.week();
 
-  for (key in data) {
-    const headerItem = { 'type': 'header', 'data': { 'text': `Compare with ${(key === 'year')?'':key + ' of'} last year`, 'level': 3 } };
-    const tableItem = { 'type': 'table', 'data': { 'content': [] } };
+  for (const key in data) {
+    const headerItem = { type: 'header', data: { text: `Compare with ${(key === 'year') ? '' : `${key} of`} last year`, level: 3 } };
+    const tableItem = { type: 'table', data: { content: [] } };
     const thArray = [];
     const tdArray = [];
-    for (dayKey in data[key]) {
-      for (subKey in data[key][dayKey]) {
+    for (const dayKey in data[key]) {
+      for (const subKey in data[key][dayKey]) {
         let txt = '';
         switch (key) {
           case 'day':
@@ -343,17 +332,16 @@ async function storeBlogItem(dbClient, data) {
     source.blocks.push(headerItem, tableItem);
   }
 
-  await insertItem(dbClient, blogId, title, source, blogAuthor, publishDate, picture, []);
+  await insertItem(dbClient, title, source, blogAuthor, publishDate, picture, []);
 }
 
-function insertItem(dbClient, blogId, title, source, author, publishDate, picture, tags) {
+function insertItem(dbClient, title, source, author, publishDate, picture, tags) {
   let content = '';
   edjsParser.parse(source).forEach((item) => {
     content += item;
   });
   const slug = slugify(title, { lower: true, strict: true });
   const blog = {
-    _id: blogId,
     type: 'blog',
     info: {
       author: {
@@ -376,7 +364,7 @@ function insertItem(dbClient, blogId, title, source, author, publishDate, pictur
     },
   };
 
-  dbClient.db().collection('items').insertOne(blog);
+  return dbClient.db().collection('items').insertOne(blog);
 }
 
 exports.scheduleParsing = scheduleParsing;
