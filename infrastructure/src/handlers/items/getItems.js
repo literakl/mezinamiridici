debe5aa48c4aca80a0c518bec17d0f615b53bdc7
Leos Literak <leos@literak.cz>
2020-11-01 19:08:11 +0100

    do no send data property

--- infrastructure/src/handlers/items/getItems.js
+++ infrastructure/src/handlers/items/getItems.js
@@ -33,6 +33,7 @@ function getItems(dbClient, start, num, tag) {
 
   return dbClient.db().collection('items')
     .find(query)
+    .project({ type: 1, info: 1, comments: 1 })
     .sort({ 'info.date': -1 })
     .skip(start)
     .limit(num)
