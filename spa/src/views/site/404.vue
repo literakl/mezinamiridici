706c56fcf44c82b9d3c962ee9292d167ba66d1dd
Leos Literak <leos@literak.cz>
2020-11-14 11:27:01 +0100

    changed image and resources structure

--- spa/src/views/site/404.vue
+++ spa/src/views/site/404.vue
@@ -4,7 +4,7 @@
       <b-col>
         <h1>{{ $t('generic.not-found-caption') }}</h1>
         <p>{{ $t('generic.not-found-message') }}</p>
-        <img src="/img/opravit.jpg" :alt="$t('generic.we-must-fix')" width="800">
+        <img src="/images/opravit.jpg" :alt="$t('generic.we-must-fix')" width="800">
       </b-col>
     </b-row>
   </div>
