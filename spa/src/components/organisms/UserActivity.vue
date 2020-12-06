a766042e0892908aac4791ffb2af3cf727b4e639
Leos Literak <leos@literak.cz>
2020-12-06 14:04:23 +0100

    #77 - implemented without InfiniteGrid with a button Load more.

--- spa/src/components/organisms/UserActivity.vue
+++ spa/src/components/organisms/UserActivity.vue
@@ -1,28 +1,45 @@
 <template>
-  <GridLayout
-    ref="ig"
-    :options="options"
-    :layoutOptions="layoutOptions"
-    @append="onAppend"
-    @layout-complete="onLayoutComplete"
-  >
-    <div v-for="item in list" :key="item._id" :groupKey="item.groupKey">
-      <Date :date="item.info.date" format="dynamicDate" />
-      <router-link :to="{ name: 'blog', params: { slug: item.info.slug, id: userId } }">
-        {{ item.info.caption }}
-      </router-link>
+  <div>
+    <div class="row pb-3">
+      <Radio
+        class="pl-3"
+        v-model="filter"
+        :label="$t('profile.filter.posts')"
+        name="filter"
+        identifier="blog"/>
+      <Radio
+        class="pl-3"
+        v-model="filter"
+        :label="$t('profile.filter.comments')"
+        name="filter"
+        identifier="comment"/>
     </div>
-  </GridLayout>
+
+    <div v-for="item in list" :key="item._id">
+      <Date :date="item.info.date" format="dynamicDate" /> &nbsp;
+      <router-link :to="getURL(item)">{{ item.info.caption }}</router-link>
+    </div>
+
+    <Button
+      v-if="!hasEnded"
+      size="sm"
+      class="w-25"
+      :value="$t('generic.loading-more-button')"
+      @clicked="fetchActivity(true)"
+    />
+  </div>
 </template>
 
 <script>
-import { GridLayout } from '@egjs/vue-infinitegrid';
 import Date from '@/components/atoms/Date.vue';
+import Button from '@/components/atoms/Button.vue';
+import Radio from '@/components/atoms/Radio.vue';
 
 export default {
   name: 'UserActivity',
   components: {
-    GridLayout,
+    Button,
+    Radio,
     Date,
   },
   props: {
@@ -31,52 +48,36 @@ export default {
   data() {
     return {
       filter: 'blog',
-      start: 0,
+      index: 0,
       pageSize: 10,
       hasEnded: false,
       list: [],
-      options: {
-        // isOverflowScroll: false,
-        // useFit: true,
-        // useRecycle: true,
-        isConstantSize: true,
-        horizontal: false,
-        align: 'center',
-        transitionDuration: 0.2,
-      },
-      layoutOptions: {
-        margin: 15,
-        align: 'center',
-      },
     };
   },
+  watch: {
+    filter() {
+      this.fetchActivity(false);
+    },
+  },
+  created() {
+    this.fetchActivity(false);
+  },
   methods: {
-    async onAppend({ groupKey, startLoading }) {
-      if (this.$refs.ig.isProcessing()) {
-        return;
-      }
-      if (this.hasEnded) {
-        return;
-      }
-
-      startLoading();
-      const payload = { start: this.start, size: this.pageSize, userId: this.userId, type: this.filter };
+    async fetchActivity(append) {
+      const payload = { start: this.index, size: this.pageSize, userId: this.userId, type: this.filter };
       const items = await this.$store.dispatch('FETCH_USER_ACTIVITY', payload);
       if (items.length === 0) {
         this.hasEnded = true;
-        this.$refs.ig.endLoading();
         return;
+      } else if (items.length < this.pageSize) {
+        this.hasEnded = true;
       }
 
-      this.start = this.start + this.pageSize;
-      const newGroupKey = parseFloat(groupKey || 0) + 1;
-      items.forEach((item) => { item.groupKey = newGroupKey; });
-      this.list = this.list.concat(items);
+      this.index = this.index + this.pageSize;
+      this.list = (append) ? this.list.concat(items) : items;
     },
-    onLayoutComplete({ isLayout, endLoading }) {
-      if (!isLayout) {
-        endLoading();
-      }
+    getURL(item) {
+      return { name: 'blog', params: { slug: item.info.slug, id: this.userId } };
     },
   },
 };
