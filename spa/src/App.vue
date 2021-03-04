cd5e5d1971c8fc497362a0262065b1f60c9e6446
Akbar <27790979+donatelloraphael@users.noreply.github.com>
2021-03-04 23:50:48 +0530

    merge App.vue

--- spa/src/App.vue
+++ spa/src/App.vue
@@ -2,31 +2,28 @@
   <div>
     <header>
       <b-navbar toggleable="sm" type="dark" variant="dark">
-        <b-navbar-brand href="/">
-          <img src="/images/icons/logo.png" :alt="$t('app.logo-alt')"  class="d-inline-block align-top">
+        <b-navbar-brand href="/" class="brand-pic">
+          <img src="/images/icons/logo.png" :alt="$t('app.logo-alt')"  class="d-inline-block align-top brand">
         </b-navbar-brand>
 
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-        <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav class="d-none d-sm-block">
-            <div class="d-inline-flex flex-column">
+            <div class="d-inline-flex flex-column title-tag">
               <h2><a class="text-white" href="/">{{ $t('app.name') }}</a></h2>
               <span class="text-white">{{ $t('app.slogan') }}</span>
             </div>
           </b-navbar-nav>
 
-          <b-navbar-nav class="ml-auto align-items-center">
+          <b-navbar-nav class="ml-auto align-items-center rightmenu">
             <b-nav-item :to="{ name: 'sign-in'}" v-if="!authorized">{{ $t('app.sign-in-up') }}</b-nav-item>
             <b-nav-item-dropdown v-if="authorized" toggle-class="text-warning" right>
               <template v-slot:button-content>
-                <BIconPlus scale="2"></BIconPlus>
+                <BIconPencilSquare scale="2"></BIconPencilSquare>
               </template>
               <b-dropdown-item :to="{ name: 'create-blog', params: { id: userId } }">{{ $t('app.new-post') }}</b-dropdown-item>
             </b-nav-item-dropdown>
             <b-nav-item-dropdown v-if="authorized" toggle-class="text-warning" right>
               <template v-slot:button-content>
-                <BIconPersonFill scale="2"></BIconPersonFill>
+                <BIconPersonCircle scale="2"></BIconPersonCircle>
               </template>
               <b-dropdown-item :to="{ name: 'user-profile', params: { id: userId } }">{{ $t('app.my-profile') }}</b-dropdown-item>
               <b-dropdown-item :to="{ name: 'update-profile', params: { id: userId }}">{{ $t('app.update-profile') }}</b-dropdown-item>
@@ -34,7 +31,7 @@
             </b-nav-item-dropdown>
             <b-nav-item-dropdown toggle-class="text-warning" right>
               <template v-slot:button-content>
-                <BIconInfo scale="2"></BIconInfo>
+                <BIconInfoCircle scale="2"></BIconInfoCircle>
               </template>
               <b-dropdown-item href="/o/napoveda">{{ $t('app.help') }}</b-dropdown-item>
               <b-dropdown-item href="/o/mise">{{ $t('app.our-mission') }}</b-dropdown-item>
@@ -42,7 +39,6 @@
               <b-dropdown-item href="/o/reklama">{{ $t('app.advertisement') }}</b-dropdown-item>
             </b-nav-item-dropdown>
           </b-navbar-nav>
-        </b-collapse>
       </b-navbar>
     </header>
     <main>
@@ -52,24 +48,20 @@
 </template>
 
 <script>
-import { BIconPersonFill, BIconInfo, BIconPlus,
-  BNavbar, BNavbarNav, BNavItemDropdown, BDropdownItem,
-  BNavbarToggle, BNavbarBrand, BCollapse, BNavItem } from 'bootstrap-vue';
+import { BIconPersonCircle, BIconInfoCircle, BIconPencilSquare, BNavbar, BNavbarNav, BNavItemDropdown, BDropdownItem, BNavbarBrand, BNavItem } from 'bootstrap-vue';
 
 export default {
   name: 'App',
   components: {
-    BIconPersonFill,
-    BIconInfo,
-    BIconPlus,
+    BIconPersonCircle,
+    BIconInfoCircle,
+    BIconPencilSquare,
     BNavbar,
     BNavbarNav,
     BNavItemDropdown,
     BDropdownItem,
-    BNavbarToggle,
-    BNavbarBrand,
-    BCollapse,
     BNavItem,
+    BNavbarBrand,
   },
   computed: {
     authorized() {
