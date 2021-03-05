7d6fb90406bb29d2c9740a9c48ad3a96fe448de0
Akbar <27790979+donatelloraphael@users.noreply.github.com>
2021-03-05 20:34:24 +0530

    merge PollButtons and PollHeading

--- spa/src/components/molecules/PollButtons.vue
+++ spa/src/components/molecules/PollButtons.vue
@@ -1,48 +1,34 @@
 <template>
-  <b-container>
-    <b-row>
-      <b-col>
-        <h4 class="text-your-opinion">{{ $t('poll.your-opinion') }}</h4>
-      </b-col>
-      <b-col>
-        <b-button variant="success" v-on:click="voted('neutral')">
-          <img src="/images/icons/happy.png" class="pr-2" alt="">
-          {{ $t('poll.choices.neutral') }}
-        </b-button>
-      </b-col>
-      <b-col>
-        <b-button variant="primary" v-on:click="voted('trivial')">
-          <img src="/images/icons/ok.png" class="pr-2" alt="">
-          {{ $t('poll.choices.trivial') }}
-        </b-button>
-      </b-col>
-      <b-col>
-        <b-button variant="warning" v-on:click="voted('dislike')">
-          <img src="/images/icons/dislike.png" class="pr-2" alt="">
-          {{ $t('poll.choices.dislike') }}
-        </b-button>
-      </b-col>
-      <b-col>
-        <b-button variant="danger" v-on:click="voted('hate')">
-          <img src="/images/icons/angry.png" class="pr-2" alt="">
-          {{ $t('poll.choices.hate') }}
-        </b-button>
-      </b-col>
-    </b-row>
-  </b-container>
+  <div class="normal-poll pt-4">
+    <div class="normal-poll-inside">
+      <h4 class="pb-2">{{ $t('poll.your-opinion') }}</h4>
+      <b-button variant="success" v-on:click="voted('neutral')">
+        <img src="/images/icons/happy.png" alt="">
+        {{ $t('poll.choices.neutral') }}
+      </b-button>
+      <b-button variant="primary" v-on:click="voted('trivial')">
+        <img src="/images/icons/ok.png" alt="">
+        {{ $t('poll.choices.trivial') }}
+      </b-button>
+      <b-button variant="warning" v-on:click="voted('dislike')">
+        <img src="/images/icons/dislike.png" alt="">
+        {{ $t('poll.choices.dislike') }}
+      </b-button>
+      <b-button variant="danger" v-on:click="voted('hate')">
+        <img src="/images/icons/angry.png" alt="">
+        {{ $t('poll.choices.hate') }}
+      </b-button>
+    </div>
+  </div>
 </template>
 
 <script>
-import { BContainer, BButton, BRow, BCol } from 'bootstrap-vue';
+import { BButton } from 'bootstrap-vue';
 
 export default {
   name: 'PollButtons',
   components: {
-    BContainer,
     BButton,
-    BRow,
-    BCol,
-
   },
   methods: {
     voted(vote) {
@@ -56,8 +42,121 @@ export default {
 };
 </script>
 
-<style scoped lang="scss">
-  .text-your-opinion {
-    //font-size: x-large;
+<style scoped>
+/* Default Poll Css Start */
+.normal-poll {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
+.normal-poll-inside h4 {
+  font-size: 16px;
+  text-transform: uppercase;
+  padding: 16px 0 0 0px;
+  color: var(--text-color);
+}
+/* Default Poll Buttons Css Start */
+.normal-poll-inside {
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+}
+.normal-poll-inside button {
+  display: flex;
+  align-items: center;
+  position: relative;
+  max-width: 200px;
+  width: 100%;
+  justify-content: center;
+  text-align: center;
+  box-shadow: 0 10px 16px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19);
+  padding: 0;
+  height: 40px;
+  margin: 15px;
+  font-size: 14px;
+  font-weight: 400;
+}
+.normal-poll-inside button.btn-success img {
+  width: 60px;
+  height: 40px;
+  top: -38px;
+}
+.normal-poll-inside button img {
+  position: absolute;
+  top: -42px;
+  width: 48px;
+  height: 43px;
+  display: block;
+  margin: 0 auto;
+  right: 20%;
+  left: 20%;
+}
+.normal-poll-inside button span {
+  width: 32px;
+  height: 27px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background:rgba(255, 255, 255, .7);
+  color: var(--dark-color);
+  position: absolute;
+  left: 7px;
+  font-size: 13px;
+  box-shadow: var(--big-shadow);
+  border-radius: 3px;
+  top: 11px;
+}
+/* Default Poll Buttons Css End */
+@media (max-width: 992px) {
+  .normal-poll-inside button img {
+    left: 0;
+    right: 0;
   }
+  .normal-poll-inside button span {
+    top: 38px;
+  }
+  .normal-poll-inside button {
+    width: 105px;
+    margin: 35px 5px 30px!important;
+    font-size: 13px;
+  }
+  .normal-poll-inside h4{
+    padding: 0;
+  }
+}
+@media (max-width: 767px) {
+  .normal-poll-inside {
+    flex-wrap: wrap;
+    justify-content: space-around!important;
+    padding: 0 10px;
+  }
+  .normal-poll-inside button {
+    width: 45%;
+  }
+  .normal-poll-inside h4 {
+    flex-wrap: wrap;
+    flex-basis: 100%;
+    text-align: center;
+  }
+  .normal-poll {
+    padding-top: 0px!important;
+  }
+}
+@media (max-width: 600px) {
+  .normal-poll-inside {
+    padding: 0 0px;
+  }
+}
+
+@media (max-width: 480px) {
+  .normal-poll-inside button {
+    width: 145px;
+  }
+}
+@media (max-width: 360px) {
+  .normal-poll-inside button {
+    width: 122px;
+  }
+}
 </style>
--- spa/src/components/molecules/PollHeading.vue
+++ spa/src/components/molecules/PollHeading.vue
@@ -1,43 +1,57 @@
 <template>
-  <b-container>
-    <b-row align-v="center" align-h="center">
-      <b-col sm="1" v-if="item.siblings">
+  <div>
+    <div class="poll-header">
+      <b-col v-if="item.siblings" class="prevbtn">
         <b-button v-if="item.siblings.older" :to="link(item.siblings.older)" variant="secondary">
           <BIconChevronDoubleLeft font-scale="2"></BIconChevronDoubleLeft>
         </b-button>
       </b-col>
-      <b-col sm="auto">
-        <div class="item-div item-hover mb-3">
-          <h4>
-            <router-link :to="{ name: 'poll', params: { slug: item.info.slug }}">
-              {{item.info.caption}}
-            </router-link>
-          </h4>
-          <div class="mt-3 p-1 pl-3 item-footer">
-            <Date :date="this.item.info.date" format="dynamicDate" />  &bull;
-            <ProfileLink :profile="this.item.info.author"/> &bull;
-            {{ $t('poll.votes') }}: {{item.votes.total}} &bull;
-            <router-link :to="{ name: 'poll', params: { slug: item.info.slug }, hash: '#comments'}">
-              {{ $t('comment.comments') }}: {{item.comments.count}}
-            </router-link>
-            <template v-if="hasTags">
-              &bull;
-              <TagList :tags="tags"/>
-            </template>
+      <b-col class="center-box">
+        <div class="post-tags">
+          <template v-if="hasTags">
+            <BIconTags scale="1"></BIconTags>
+            <TagList :tags="tags"/>
+          </template>
+        </div>
+        <h4 class="pollheading">
+          <router-link :to="{ name: 'poll', params: { slug: item.info.slug }}">
+            {{item.info.caption}}
+          </router-link>
+        </h4>
+        <div class="item-div item-hover mb-4">
+          <div class="item-footer">
+            <div class="post-details">
+              <div class="post-author">
+                <BIconPersonCircle scale="1"></BIconPersonCircle>
+                <span><ProfileLink :profile="this.item.info.author"/></span>
+              </div>
+              <div class="post-time">
+                <BIconCalendarRange scale="1"></BIconCalendarRange>
+                <span class="date"><Date :date="this.item.info.date" format="dynamicDate" /></span>
+              </div>
+              <div class="post-rating">
+                <img src="/images/icons/vote-icon.svg" alt="">
+                <span> {{item.votes.total}}</span>
+              </div>
+              <div class="post-comments">
+                <BIconChatTextFill scale="1"></BIconChatTextFill>
+                <span>{{item.comments.count}}</span>
+              </div>
+            </div>
           </div>
         </div>
       </b-col>
-      <b-col sm="1" v-if="item.siblings">
+      <b-col v-if="item.siblings" class="nextbtn">
         <b-button v-if="item.siblings.newer" :to="link(item.siblings.newer)" variant="secondary">
           <BIconChevronDoubleRight font-scale="2"></BIconChevronDoubleRight>
         </b-button>
       </b-col>
-    </b-row>
-  </b-container>
+    </div>
+  </div>
 </template>
 
 <script>
-import { BIconChevronDoubleLeft, BIconChevronDoubleRight, BContainer, BRow, BCol, BButton } from 'bootstrap-vue';
+import { BIconChevronDoubleLeft, BIconChevronDoubleRight, BCol, BButton, BIconPersonCircle, BIconCalendarRange, BIconChatTextFill, BIconTags } from 'bootstrap-vue';
 import ProfileLink from '@/components/molecules/ProfileLink.vue';
 import Date from '@/components/atoms/Date.vue';
 import TagList from '@/components/atoms/TagList.vue';
@@ -50,10 +64,12 @@ export default {
     TagList,
     BIconChevronDoubleLeft,
     BIconChevronDoubleRight,
-    BContainer,
-    BRow,
     BCol,
     BButton,
+    BIconPersonCircle,
+    BIconCalendarRange,
+    BIconChatTextFill,
+    BIconTags,
   },
   props: {
     item: Object,
@@ -81,16 +97,195 @@ export default {
 </script>
 
 <style scoped>
+
+/* Poll Page Css Start */
+.poll-header{
+  flex-wrap: nowrap;
+  margin: 0 auto;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+}
+.poll-header .center-box{
+  width: 100%;
+  padding: 0;
+}
+.pollheading{
+  margin: 0;
+}
+.pollheading a{
+  font-size:21px;
+  text-decoration: none;
+  color: var(--text-color);
+  margin: 10px 0 0px;
+}
+.post-rating img{
+    width: 16px;
+    height: 15px;
+    margin-right: 8px;
+}
+.post-rating svg{
+  position: relative;
+}
+.post-rating:before{
+  content: "";
+  position: absolute;
+}
 .item-div {
-  border-width: 10px;
-  border-color: #f1f1f1;
-  border-style: solid;
-  box-shadow: #c1c1c1 1px 1px 10px;
+  width: 100%;
+  position: relative;
+  border: 0;
+  border-bottom: 1px solid #dee0e1;
+}
+.post-details{
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  font-size: 13px;
+}
+.post-time {
+  color: #777A7C;
+}
+.post-tags{
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  margin-bottom: 10px;
+}
+.post-tags svg{
+  margin-right: 8px;
 }
 .item-footer {
-  background-color: #f1f1f1;
-  font-size: 0.8em;
-  color: #201f27;
+  background-color: #fff;
+  padding: 10px 0px 5px;
+  color: #777A7C;
   font-weight: 600;
+  margin: 0px 0;
+  max-width: 280px;
+}
+.item-footer a {
+  color: #777A7C;
+}
+.item-footer svg{
+  margin-right: 8px;
+  font-size: 15px;
+}
+.post-time, .post-author, .post-rating, .post-comments{
+  display: flex;
+  align-items: center;
+  font-weight:300;
+}
+.item-footer span a{
+  color: #777A7C;
+  text-decoration: none;
+}
+.prevbtn {
+  max-width:40px;
+  position: absolute;
+  left: -50px;
+  top: 0;
+  z-index: 1;
+  background: #fff;
+  padding: 0;
+}
+.nextbtn {
+  max-width:40px;
+  position: absolute;
+  right:-50px;
+  top:0;
+  z-index: 1;
+  background: #fff;
+  padding: 0;
+}
+.center-box {
+  max-width: 100%;
+  margin: 0 auto;
+}
+.prevbtn a, .nextbtn a {
+  border-radius: 100px;
+  height: 40px;
+  width: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: transparent;
+  border: 1px solid #ddd;
+  color: #AEB3B7 !important;
+}
+.prevbtn a.btn:active, .nextbtn a.btn:active {
+  background: transparent;
+}
+.prevbtn a svg, .nextbtn a svg {
+  font-size: 140%!important;
+}
+
+@media (max-width: 1352px) {
+  .poll-header{
+    display: flex;
+  }
+  .prevbtn{
+    position: unset;
+    margin-right: 15px;
+
+  } .nextbtn {
+    position: unset;
+    margin-left: 15px;
+  }
+}
+
+@media (max-width: 992px) {
+  .nextbtn{
+    right: 0px;
+    top: 0px;
+  }
+  .prevbtn{
+    left:0px;
+  }
+}
+
+@media (max-width: 767px) {
+  .poll-header {
+    margin-bottom: 0px;
+  }
+  .prevbtn {
+    bottom: 0px;
+    top:40%;
+  }
+  .nextbtn {
+    bottom: 0px;
+    top:40%;
+  }
+}
+@media (max-width: 600px) {
+  .post-tags{
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .post-tags svg{ margin-right: 15px;}
+
+  .pollheading {
+      margin: 0;
+  }
+  .poll-header {
+    padding: 0 0px;
+  }
+}
+@media (max-width: 600px) {
+  .nextbtn {
+    width: 35px;
+  }
+  .prevbtn a, .nextbtn a {
+    height: 35px;
+    width: 35px;
+  }
+}
+@media (max-width: 350px) {
+  .post-time, .post-author, .post-rating, .post-comments {
+    flex-direction: column;
+  }
+  .item-footer svg, .post-rating img {
+    margin-right: 0;
+    margin-bottom: 10px;
+  }
 }
 </style>
