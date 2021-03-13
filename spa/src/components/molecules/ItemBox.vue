9850c6852cbd03cb218a06c67d9b411777f1880e
Leos Literak <leos@literak.cz>
2021-03-13 13:35:46 +0100

    added missing condition to hide poll symbol for other item types CSS cleanup and rename code reformatting

--- spa/src/components/molecules/ItemBox.vue
+++ spa/src/components/molecules/ItemBox.vue
@@ -4,13 +4,12 @@
       <b-img :src="item.info.picture"></b-img>
     </router-link>
     <div class="overlap-actions">
-      <!-- Item box Result Bars  -->
-      <div class="result-bars">
-        <span class="no-prob"></span>
-        <span class="trival-trouble"></span>
-        <span class="donot-like"></span>
+      <div v-if="item.type === 'poll'" class="poll-symbol">
+        <span class="poll-neutral"></span>
+        <span class="poll-trival"></span>
+        <span class="poll-dislike"></span>
       </div>
-      <span class="data"><Date :date="item.info.date" format="dynamicDate" /></span>
+      <span class="date"><Date :date="item.info.date" format="dynamicDate"/></span>
     </div>
     <h4>
       <router-link :to="link">
@@ -20,12 +19,12 @@
     <div class="py-2 px-2 item-footer">
       <div class="bottom-links">
         <template v-if="showAuthor"><span><BIconPersonCircle></BIconPersonCircle> <ProfileLink :profile="item.info.author"/></span></template>
-        <template v-if="item.type === 'poll'">{{ $t('poll.votes') }}: {{item.votes_count}}</template>
+        <template v-if="item.type === 'poll'">{{ $t('poll.votes') }}: {{ item.votes_count }}</template>
         <template v-if="hasDiscussion">
           <span>
             <BIconChatTextFill></BIconChatTextFill>
             <router-link :to="commentLink">
-              {{item.comments.count}}
+              {{ item.comments.count }}
             </router-link>
           </span>
         </template>
@@ -60,13 +59,25 @@ export default {
   computed: {
     link() {
       if (this.item.type === 'poll') {
-        return { name: 'poll', params: { slug: this.item.info.slug } };
+        return {
+          name: 'poll',
+          params: { slug: this.item.info.slug },
+        };
       }
       if (this.item.type === 'blog') {
-        return { name: 'blog', params: { slug: this.item.info.slug, id: this.item.info.author.id } };
+        return {
+          name: 'blog',
+          params: {
+            slug: this.item.info.slug,
+            id: this.item.info.author.id,
+          },
+        };
       }
       if (this.item.type === 'page') {
-        return { name: 'page', params: { slug: this.item.info.slug } };
+        return {
+          name: 'page',
+          params: { slug: this.item.info.slug },
+        };
       }
       return { name: 'home' };
     },
@@ -86,68 +97,74 @@ export default {
 </script>
 <style scoped>
 
-  .item-div {
-    border-color: #f1f1f1;
-    border-style: solid;
-    box-shadow: #c1c1c1 1px 1px 10px;
-    border: 0;
-  }
+.item-div {
+  box-shadow: #c1c1c1 1px 1px 10px;
+  border: 0 solid #f1f1f1;
+}
+
 .item-hover {
-  cursor:pointer;
+  cursor: pointer;
   transition: 0.2s ease;
 }
+
 .item-hover:hover {
   transform: translateX(-2px) translateY(-2px) scale(1.03);
 }
+
 .item-div img {
   box-shadow: var(--big-shadow);
   border: 5px solid #fff;
   width: 100%;
 }
+
 .item-div h4 {
   text-align: center;
   height: 55px;
   margin: 0;
   overflow: hidden;
 }
+
 .item-footer {
   position: relative;
 }
+
 .item-div:hover h4, .item-footer {
-    width: 100%;
-    bottom: 0;
+  width: 100%;
+  bottom: 0;
 }
+
 .item-div h4 a {
   color: var(--dark-color);
-  text-align: center;
   font-size: 16px;
   padding: 16px 10px 18px 10px;
-  display: block;
   display: flex;
   align-items: flex-start;
   justify-content: flex-start;
   text-align: left;
   text-overflow: ellipsis;
 }
+
 .item-div h4 a:hover {
   text-decoration: none;
 }
+
 .overlap-actions {
   position: relative;
 }
-/* Item box Results Bars for Home infinite posts */
-.result-bars {
+
+.poll-symbol {
   position: absolute;
   display: block;
   width: 100%;
   top: -35px;
-  left:5px;
+  left: 5px;
 }
-.result-bars span {
+
+.poll-symbol span {
   margin-top: 1px;
 }
 
-.no-prob {
+.poll-neutral {
   height: 9px;
   background: rgb(191 229 200);
   width: 7%;
@@ -156,7 +173,7 @@ export default {
   display: block;
 }
 
-.trival-trouble {
+.poll-trival {
   height: 9px;
   background: rgb(179 216 255);
   width: 4.5%;
@@ -164,28 +181,31 @@ export default {
   border-radius: 0 6px 6px 0;
   display: block;
 }
-.donot-like {
+
+.poll-dislike {
   height: 9px;
-  background: rgb(255 237 181/1);
+  background: rgb(255 237 181);
   width: 10.5%;
   left: 5px;
   border-radius: 0 6px 6px 0;
   display: block;
 }
-span.data {
+
+span.date {
   position: absolute;
-  right: 0px;
   font-size: 12px;
   font-weight: 400;
   right: 0;
   background: #fff;
-  padding: 0px 10px;
-  color:#777A7C;
-  top:-20px;
+  padding: 0 10px;
+  color: #777A7C;
+  top: -20px;
 }
-.item-footer:hover span.data {
+
+.item-footer:hover span.date {
   opacity: 1;
 }
+
 .bottom-links {
   display: flex;
   width: 100%;
@@ -198,19 +218,23 @@ span.data {
 }
 
 @media (min-width: 1920px) {
-  span.data {
+  span.date {
     top: -20px;
   }
-  .item-footer span.data span {
-    font-size: 13px!important;
+
+  .item-footer span.date span {
+    font-size: 13px !important;
   }
+
   .bottom-links span svg {
     font-size: 20px;
   }
+
   .item {
     width: 400px;
   }
 }
+
 @media (max-width: 767px) {
   .item-div h4 {
     height: auto;
