0650e6728549ebb8530889eb8b78422f8db274ef
Leos Literak <leos@literak.cz>
2021-08-15 21:27:18 +0200

    #103 - moved compiled in configuration into runtime configuration

---
+++ spa/public/runtimeConfig.js
@@ -0,0 +1,9 @@
+window.config = {
+  I18N_LOCALE: 'cs',
+  I18N_FALLBACK_LOCALE: 'cs',
+  LOG_LEVEL: 'debug',
+  LOG_PRODUCTION_TIP: false,
+  API_ENDPOINT: 'http://localhost:3000/v1',
+  BFF_ENDPOINT: 'http://localhost:3000/bff',
+  REPLY_LIMIT: 5,
+};
