fd53d37b3c79a8618793143e49476b91f535530a
Leos Literak <leos@literak.cz>
2021-10-05 08:32:59 +0200

    #221 code review

--- backend/src/handlers/snippets/snippets.js
+++ backend/src/handlers/snippets/snippets.js
@@ -7,7 +7,7 @@ module.exports = (app) => {
   app.options('/v1/posts/:blogId/snippets', auth.cors);
   app.options('/v1/posts/:blogId/snippets/:code', auth.cors);
 
-  app.get('/v1/posts/:blogId/snippets', auth.required, auth.staffer, auth.cors, async ( req, res ) => {
+  app.get('/v1/posts/:blogId/snippets', auth.required, auth.staffer, auth.cors, async (req, res) => {
     logger.debug('get snippets handler starts');
     // find all documents in snippets collection where itemId is blogId
     const { blogId } = req.params;
@@ -29,11 +29,11 @@ module.exports = (app) => {
     }
   });
 
-  app.post('/v1/posts/:blogId/snippets', auth.required, auth.editor_in_chief, auth.cors, async (req, res ) => {
+  app.post('/v1/posts/:blogId/snippets', auth.required, auth.editor_in_chief, auth.cors, async (req, res) => {
     logger.debug('create snippet handler starts');
     // insert new document into snippets collection with itemId set to blogId
     const {
-      code, type, content, date
+      code, type, content, date,
     } = req.body;
 
     const { blogId } = req.params;
@@ -48,38 +48,37 @@ module.exports = (app) => {
     }
     if (!content) {
       return api.sendMissingParam(res, 'content');
-    } 
+    }
     const publishDate = new Date();
     if (!publishDate) {
       return api.sendInvalidParam(res, 'date', date);
     }
 
-    try{
+    try {
       const dbClient = await mongo.connectToDatabase();
       const user = auth.getIdentity(req.identity);
       const snippetId = mongo.generateTimeId();
 
       const blog = await mongo.getBlog(dbClient, undefined, blogId);
       logger.debug('Blog fetched');
-      
-      await insertItem(dbClient,snippetId,blog._id,code,user,publishDate,type,content);
+
+      await insertItem(dbClient, snippetId, blog._id, code, user, publishDate, type, content);
       logger.debug('Snippet inserted');
-      
+
       const snippet = await getSnippet(dbClient, snippetId);
       logger.debug('Snippet fetched');
 
       return api.sendCreated(res, api.createResponse(snippet));
-    }
-    catch(err) {
+    } catch (err) {
       logger.error('Request failed', err);
       return api.sendInternalError(res, api.createError('Failed to create snippet', 'sign-in.something-went-wrong'));
     }
   });
 
-  app.patch('/v1/posts/:blogId/snippets/:code', auth.required, auth.editor_in_chief, auth.cors, async ( req, res ) => {
+  app.patch('/v1/posts/:blogId/snippets/:code', auth.required, auth.editor_in_chief, auth.cors, async (req, res) => {
     logger.debug('update snippet handler starts');
     // update document from snippets collection with itemId set to blogId and given code
-    const { blogId,code } = req.params;
+    const { blogId, code } = req.params;
     if (!blogId) {
       return api.sendMissingParam(res, 'blogId');
     }
@@ -101,10 +100,10 @@ module.exports = (app) => {
     }
   });
 
-  app.delete('/v1/posts/:blogId/snippets/:code', auth.required, auth.editor_in_chief, auth.cors, async ( req, res ) => {
+  app.delete('/v1/posts/:blogId/snippets/:code', auth.required, auth.editor_in_chief, auth.cors, async (req, res) => {
     logger.debug('delete snippet handler starts');
     // delete document from snippets collection with itemId set to blogId and given code
-    const { blogId,code } = req.params;
+    const { blogId, code } = req.params;
     if (!blogId) {
       return api.sendMissingParam(res, 'blogId');
     }
@@ -120,53 +119,52 @@ module.exports = (app) => {
         return api.sendResponse(res, api.createResponse(result));
       }
       return api.sendResponse(res, api.createError('Snippets not found', 'generic.not-found-caption'));
-      
     } catch (err) {
       logger.error('Request failed', err);
       return api.sendInternalError(res, api.createError('Failed to delete snippets', 'sign-in.something-went-wrong'));
     }
   });
 };
-async function insertItem(dbClient,snippetId,blogId,code,user,date,type,content){
-const snippet={
-  _id: snippetId,
-  itemId: blogId,
-  code:code,
-  user:{
-    nickname:user.nickname,
-    id:user.userId
-  },
-  date:date,
-  type:type,
-  content:content,
-}
-return dbClient.db().collection('snippets').insertOne(snippet);
+async function insertItem(dbClient, snippetId, blogId, code, user, date, type, content) {
+  const snippet = {
+    _id: snippetId,
+    itemId: blogId,
+    code,
+    user: {
+      nickname: user.nickname,
+      id: user.userId,
+    },
+    date,
+    type,
+    content,
+  };
+  return dbClient.db().collection('snippets').insertOne(snippet);
 }
 
-async function getSnippet(dbClient, snippetId){
+async function getSnippet(dbClient, snippetId) {
   if (snippetId) {
     return dbClient.db().collection('snippets').findOne({ _id: snippetId });
   }
   throw new Error('Snippet id is empty');
 }
 
-async function getAllSnippets(dbClient, blogId){
+async function getAllSnippets(dbClient, blogId) {
   if (blogId) {
     return dbClient.db().collection('snippets').find({ itemId: blogId }).toArray();
   }
   throw new Error('blog id is empty');
 }
 
-async function updateSnippet(dbClient, blogId, code){
+async function updateSnippet(dbClient, blogId, code) {
   if (blogId) {
-    return dbClient.db().collection('snippets').findOneAndUpdate({ itemId: blogId},{$set: { code : code}});
+    return dbClient.db().collection('snippets').findOneAndUpdate({ itemId: blogId }, { $set: { code } });
   }
   throw new Error('blog id is empty');
 }
 
-async function deleteSnippet(dbClient, blogId, code){
+async function deleteSnippet(dbClient, blogId, code) {
   if (blogId) {
-    return dbClient.db().collection('snippets').deleteOne({ itemId: blogId, code: code});
+    return dbClient.db().collection('snippets').deleteOne({ itemId: blogId, code });
   }
   throw new Error('blog id is empty');
 }
--- backend/test/snippets.int.test.js
+++ backend/test/snippets.int.test.js
@@ -12,7 +12,7 @@ const {
   api, getAuthHeader,
 } = require('./testUtils');
 const {
-  setup, Leos, Jiri, Vita, Lukas
+  setup, Leos, Jiri, Vita, Lukas,
 } = require('./prepareUsers');
 
 let dbClient, server;
@@ -21,97 +21,93 @@ test('Item snippets', async (done) => {
   await setup(dbClient, api);
   jest.setTimeout(180 * 60000);
 
-    const blogBody = {
-      title: 'First blog',
-      source: '<h1>Title</h1><p>Very smart topic</p>',
-      picture: 'picture.png',
-    };
+  const blogBody = {
+    title: 'First blog',
+    source: '<h1>Title</h1><p>Very smart topic</p>',
+    picture: 'picture.png',
+  };
 
-    let blog = await api('posts', { method: 'POST', json: blogBody, headers: getAuthHeader(Vita.jwt) }).json();
-    expect(blog.success).toBeTruthy();
+  const blog = await api('posts', { method: 'POST', json: blogBody, headers: getAuthHeader(Vita.jwt) }).json();
+  expect(blog.success).toBeTruthy();
 
-    response = await api(`posts/${blog.data._id}/snippets`, { method: 'GET', headers: getAuthHeader(Jiri.jwt)}).json();
-    expect(response.success).toBe(false)
-    expect(response.errors[0].message).toBe('Access denied');
+  // get snippets as Jiri - forbidden (missing role staffer)
+  let response = await api(`posts/${blog.data._id}/snippets`, { method: 'GET', headers: getAuthHeader(Jiri.jwt) }).json();
+  expect(response.success).toBe(false);
+  expect(response.statusCode).toBe(403);
 
-    // response = await api(`posts/${blog.data._id}/snippets`, { method: 'GET', headers: getAuthHeader(Lukas.jwt)}).json();
-    // expect(response.success).toBe(false)
-    // expect(response.errors[0].message).toBe('Access denied');
+  // get snippets as Lukas - forbidden (different user than owner)
+  // response = await api(`posts/${blog.data._id}/snippets`, { method: 'GET', headers: getAuthHeader(Lukas.jwt)}).json();
+  // expect(response.success).toBe(false)
+  // expect(response.statusCode).toBe(403);
 
-    response = await api(`posts/${blog.data._id}/snippets`, { method: 'GET', headers: getAuthHeader(Vita.jwt)}).json();
-    expect(response.success).toBe(true)
-    expect(response.data.length).toBe(0);
+  // get snippets as Vita - empty
+  response = await api(`posts/${blog.data._id}/snippets`, { method: 'GET', headers: getAuthHeader(Vita.jwt) }).json();
+  expect(response.success).toBe(true);
+  expect(response.data.length).toBe(0);
 
-    const snippetABody = {
-      code: 'set_x',
-      type: 'html',
-      content: '<javascript>x=1</javascript>',
-      date: '2021-08-16T05:22:33.121Z',
-    };
+  const snippetABody = {
+    code: 'set_x',
+    type: 'html',
+    content: '<javascript>x=1</javascript>',
+    date: '2021-08-16T05:22:33.121Z',
+  };
 
-    response = await api(`posts/${blog.data._id}/snippets`, { method: 'POST', json: snippetABody, headers: getAuthHeader(Jiri.jwt)}).json();
-    expect(response.success).toBe(false);
-    expect(response.errors[0].message).toBe('Access denied');
+  // create snippet A as Jiri - forbidden (missing role editor in chief)
+  response = await api(`posts/${blog.data._id}/snippets`, { method: 'POST', json: snippetABody, headers: getAuthHeader(Jiri.jwt) }).json();
+  expect(response.success).toBe(false);
+  expect(response.statusCode).toBe(403);
 
-    response = await api(`posts/${blog.data._id}/snippets`, { method: 'POST', json: snippetABody, headers: getAuthHeader(Vita.jwt)}).json();
-    expect(response.success).toBe(false);
-    expect(response.errors[0].message).toBe('Access denied');
+  // create snippet A as Vita - forbidden (missing role editor in chief)
+  response = await api(`posts/${blog.data._id}/snippets`, { method: 'POST', json: snippetABody, headers: getAuthHeader(Vita.jwt) }).json();
+  expect(response.success).toBe(false);
+  expect(response.statusCode).toBe(403);
 
-    response = await api(`posts/${blog.data._id}/snippets`, { method: 'POST', json: snippetABody, headers: getAuthHeader(Leos.jwt)}).json();
-    expect(response.success).toBe(true);
+  // create snippet A as Leos
+  response = await api(`posts/${blog.data._id}/snippets`, { method: 'POST', json: snippetABody, headers: getAuthHeader(Leos.jwt) }).json();
+  expect(response.success).toBe(true);
 
-    const snippetCBody = {
-      code: 'set_x',
-      type: 'html',
-      content: '<javascript>x=1</javascript>',
-      date: '2021-08-16T05:22:33.121Z',
-    };
+  const snippetCBody = {
+    code: 'set_x',
+    type: 'html',
+    content: '<javascript>x=1</javascript>',
+    date: '2021-08-16T05:22:33.121Z',
+  };
 
-    response = await api(`posts/${blog.data._id}/snippets`, { method: 'POST', json: snippetCBody, headers: getAuthHeader(Leos.jwt)}).json();
-    expect(response.success).toBeTruthy();
+  // create snippet C as Leos
+  response = await api(`posts/${blog.data._id}/snippets`, { method: 'POST', json: snippetCBody, headers: getAuthHeader(Leos.jwt) }).json();
+  expect(response.success).toBeTruthy();
 
-    response = await api(`posts/${blog.data._id}/snippets`, { method: 'GET', headers: getAuthHeader(Vita.jwt)}).json();
-    expect(response.success).toBe(true)
-    expect(response.data.length).toBe(2);
-  
-   response = await api(`posts/${blog.data._id}/snippets/set_Y`, { method: 'PATCH', headers: getAuthHeader(Leos.jwt)}).json();
-   expect(response.success).toBe(true);
-   expect(response.data.value.code).toBe('set_x');
+  // get snippets - A,C as Vita
+  response = await api(`posts/${blog.data._id}/snippets`, { method: 'GET', headers: getAuthHeader(Vita.jwt) }).json();
+  expect(response.success).toBe(true);
+  expect(response.data.length).toBe(2);
+  // compare the content
 
-   const snippetDBody = {
+  // rename snippet C to B as Leos
+  response = await api(`posts/${blog.data._id}/snippets/set_Y`, { method: 'PATCH', headers: getAuthHeader(Leos.jwt) }).json();
+  expect(response.success).toBe(true);
+  expect(response.data.value.code).toBe('set_x');
+
+  const snippetDBody = {
     code: 'set_F',
     type: 'html',
     content: '<javascript>x=1</javascript>',
     date: '2021-08-16T05:22:33.121Z',
   };
 
-  response = await api(`posts/${blog.data._id}/snippets`, { method: 'POST', json: snippetDBody, headers: getAuthHeader(Leos.jwt)}).json();
+  // add snippet D as Leos
+  response = await api(`posts/${blog.data._id}/snippets`, { method: 'POST', json: snippetDBody, headers: getAuthHeader(Leos.jwt) }).json();
   expect(response.success).toBeTruthy();
-  
 
-  response = await api(`posts/${blog.data._id}/snippets/set_x`, { method: 'DELETE', headers: getAuthHeader(Leos.jwt)}).json();
+  // remove snippet A as Leos
+  response = await api(`posts/${blog.data._id}/snippets/set_x`, { method: 'DELETE', headers: getAuthHeader(Leos.jwt) }).json();
   expect(response.success).toBe(true);
 
-  response = await api(`posts/${blog.data._id}/snippets`, { method: 'GET', headers: getAuthHeader(Vita.jwt)}).json();
+  // get snippets - B,D as Vita
+  response = await api(`posts/${blog.data._id}/snippets`, { method: 'GET', headers: getAuthHeader(Vita.jwt) }).json();
   expect(response.success).toBe(true);
   expect(response.data.length).toBe(2);
-
-
-  /*
-  1. create blog as Vita
-  2. get snippets as Jiri - forbidden (missing role staffer)
-  2. get snippets as Lukas - forbidden (different user than owner)
-  2. get snippets as Vita - empty
-  3. create snippet A as Jiri - forbidden (missing role editor in chief)
-  3. create snippet A as Vita - forbidden (missing role editor in chief)
-  3. create snippet A as Leos
-  4. create snippet C as Leos
-  5. get snippets - A,C as Vita
-  6. rename snippet C to B as Leos
-  7. add snippet D as Leos
-  8. remove snippet A as Leos
-  9. get snippets - B,D as Vita
-  */
+  // check the snippets codes
 
   done();
 });
