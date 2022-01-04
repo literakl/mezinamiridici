7dfa288d2aa62076c6b71763b68cac2df00a362f
Leos Literak <leos@literak.cz>
2022-01-04 07:59:30 +0100

    zobrazovat naši misi

---
+++ spa/src/components/info/Mise.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>
+      Cílem stránek je prevence dopravních nehod skrze výměnu názorů a vzdělávání.
+      <router-link to="/o/mise">Naše mise</router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InfoBoxContent',
+};
+</script>
+
+<style scoped>
+p {
+  font-size: 1rem;
+}
+a {
+  color: var(--link-blue);
+}
+</style>
