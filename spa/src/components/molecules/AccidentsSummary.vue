9df7de387dc73ce9159177ef7d73addf7779e58d
Leos Literak <leos@literak.cz>
2022-04-18 10:15:28 +0200

    show date from the result set replace bootstrap-vue components with plain div

--- spa/src/components/molecules/AccidentsSummary.vue
+++ spa/src/components/molecules/AccidentsSummary.vue
@@ -1,38 +1,36 @@
 <template>
-<div v-if="summary">
-  <b-container class="container mb-3" >
-  <b-row class="py-2">
-    <b-col class="m-auto" md="3">
-      <div class="font-12">27.1</div>
-    </b-col>
-    <b-col class="m-auto" md="6">
+<div v-if="summary" class="container mb-3" >
+  <div class="row py-2">
+    <div class="col col-md-3 m-auto">
+      <Date :date="summary.date" format="dynamicDate" />
+    </div>
+    <div class="col col-md-6 m-auto">
       <div class="d-flex justify-content-around">
         <div>
-        <img src="/images/icons/crash.png" width="48" height="48" alt="Počet nehod">
-        <span class="text-warning font-num"><span class="mx-2">+</span>{{ summary.total.count }}</span>
+          <img src="/images/icons/crash.png" width="48" height="48" alt="Počet nehod">
+          <span class="text-warning font-num"><span class="mx-2">+</span>{{ summary.total.count }}</span>
         </div>
         <div>
-        <img src="/images/icons/tombstone.png" width="48" height="48" alt="Počet úmrtí">
-        <span class="text-warning font-num" > <span class="mx-2" > +</span>{{ summary.total.impact.deaths }}</span>
+          <img src="/images/icons/tombstone.png" width="48" height="48" alt="Počet úmrtí">
+          <span class="text-warning font-num" > <span class="mx-2" > +</span>{{ summary.total.impact.deaths }}</span>
         </div>
         <div>
-        <img src="/images/icons/ambulance.png" width="48" height="48" alt="Počet zranění">
-        <span class="text-warning font-num"><span class="mx-2">+</span>{{ summary.total.impact.severely + summary.total.impact.slightly }}</span>
+          <img src="/images/icons/ambulance.png" width="48" height="48" alt="Počet zranění">
+          <span class="text-warning font-num"><span class="mx-2">+</span>{{ summary.total.impact.severely + summary.total.impact.slightly }}</span>
         </div>
       </div>
-    </b-col>
-    <b-col class="m-auto font-12" md="3">
-      <div class="float-right">Vice statistik <BIconArrowRight scale="1" variant="dark"></BIconArrowRight></div>
-      </b-col>
-  </b-row>
-</b-container>
+    </div>
+    <div class="col col-md-3 m-auto">
+      <!--<div class="float-right">Vice statistik <BIconArrowRight scale="1" variant="dark"></BIconArrowRight></div>-->
+    </div>
+  </div>
 </div>
 </template>
 
 <script>
 
 import Date from '@/components/atoms/Date';
-import { BIconChatTextFill, BIconCollection,BContainer, BRow, BCol, BIconArrowRight} from 'bootstrap-vue';
+import { BIconChatTextFill, BIconCollection, BIconArrowRight} from 'bootstrap-vue';
 
 export default {
   name: 'AccidentsSummary',
@@ -41,8 +39,6 @@ export default {
     BIconCollection,
     BIconArrowRight,
     Date,
-    'b-container': BContainer,
-    BRow, BCol
   },
   data() {
     return {
@@ -51,7 +47,6 @@ export default {
   },
   async created() {
     this.summary = await this.$store.dispatch('GET_ACCIDENTS_SUMMARY');
-    console.log(this.summary);//todo remove
   }
 };
 </script>
@@ -60,9 +55,6 @@ export default {
 .container {
   max-width: 1280px;
    background-color:rgba(196,196,196,.2);
-}
-.font-12 {
-  font-size: 12px;
 }
 .font-num {
   font-size: 1.7em;
